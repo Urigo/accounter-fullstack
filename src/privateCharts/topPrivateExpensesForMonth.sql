17bb5e6eb96bf98bb1d94561aaa4ce59440d97d8
Uri Goldshtein <uri.goldshtein@gmail.com>
2020-07-19 16:53:45 +0300

    improve expenses scripts

--- src/privateCharts/topPrivateExpensesForMonth.sql
+++ src/privateCharts/topPrivateExpensesForMonth.sql
@@ -7,6 +7,9 @@ FROM top_private_expenses_for_month('2020-04-01');
 select *
 FROM top_private_expenses_for_month('2020-05-01');
 
+select *
+FROM top_private_expenses_for_month('2020-06-01');
+
 
 CREATE OR REPLACE FUNCTION top_private_expenses_for_month(month_input varchar)
     RETURNS TABLE
@@ -29,5 +32,34 @@ WHERE (account_number = 9217 OR account_number = 410915)
   --       event_date IS NULL
 GROUP BY personal_category
 ORDER BY SUM(event_amount_in_usd_with_vat_if_exists)::numeric(9, 2) NULLS LAST;
+
+$$;
+
+
+select *
+FROM expenses_by_category_for_month('2020-05-01', 'love');
+
+
+CREATE OR REPLACE FUNCTION expenses_by_category_for_month(month_input varchar, personal_category_input varchar)
+    RETURNS TABLE
+            (
+                sum         numeric(9, 2),
+                description varchar
+            )
+    LANGUAGE SQL
+AS
+$$
+
+SELECT event_amount_in_usd_with_vat_if_exists,
+       bank_description
+FROM formatted_merged_tables
+WHERE personal_category = personal_category_input
+  and (account_number = 9217 OR account_number = 410915)
+  AND personal_category != 'creditcard'
+--   AND event_amount < 0
+  AND event_date >= date_trunc('month', month_input::date)
+  AND event_date <= (date_trunc('month', month_input::date) + interval '1 month' - interval '1 day')::date
+  --       event_date IS NULL
+ORDER BY event_amount_in_usd_with_vat_if_exists::numeric(9, 2) NULLS LAST;
 
 $$;
