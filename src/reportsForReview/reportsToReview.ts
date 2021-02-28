e83231d38b2b6020444e5bd548a7eeb2b03a24ef
Uri Goldshtein <uri.goldshtein@gmail.com>
2021-02-28 10:34:18 +0200

    report improvements

--- src/reportsForReview/reportsToReview.ts
+++ src/reportsForReview/reportsToReview.ts
@@ -9,7 +9,7 @@ export const reportToReview = async (query: any): Promise<string> => {
   if (query.month) {
     reportMonthToReview = `2020-0${query.month}-01`;
   } else {
-    reportMonthToReview = `2020-12-01`;
+    reportMonthToReview = `2021-01-01`;
   }
 
   const lastInvoiceNumbersQuery = readFileSync(
@@ -23,7 +23,7 @@ export const reportToReview = async (query: any): Promise<string> => {
     pool.query(
       `
       select *
-      from get_unified_tax_report_of_month('2020-01-01', '2020-12-01')
+      from get_unified_tax_report_of_month('2020-01-01', '2021-01-01')
       order by to_date(תאריך_3, 'DD/MM/YYYY'), original_id, פרטים, חשבון_חובה_1, id;
       `
     ),
@@ -40,9 +40,9 @@ export const reportToReview = async (query: any): Promise<string> => {
       <tr>
         <td>${transaction.tax_invoice_number}</td>
         <td>${transaction.event_date
-        .toISOString()
-        .replace(/T/, ' ')
-        .replace(/\..+/, '')}</td>
+          .toISOString()
+          .replace(/T/, ' ')
+          .replace(/\..+/, '')}</td>
         <td>${transaction.financial_entity}</td>
         <td>${transaction.user_description}</td>
         <td>${transaction.event_amount}</td>
@@ -115,6 +115,12 @@ export const reportToReview = async (query: any): Promise<string> => {
 
     const generateTaxFunctionCall = `onClick='generateTaxMovements("${transaction.id}");'`;
     const sendToHashavshevetFunctionCall = `onClick='sendToHashavshevet("${transaction.id}");'`;
+    const movementOrBank = transaction.פרטים && transaction.פרטים == '0';
+    const missingHashavshevetSync =
+      (movementOrBank &&
+        !transaction.hashavshevet_id &&
+        transaction.חשבון_חובה_1 != 'כא') ||
+      (!movementOrBank && !transaction.hashavshevet_id);
 
     // let url = `https://www.boi.org.il/currency.xml?rdate=${valueDate.format(
     //   'YYYYMMDD'
@@ -142,40 +148,43 @@ export const reportToReview = async (query: any): Promise<string> => {
     // })();
 
     reportToReviewHTMLTemplate = reportToReviewHTMLTemplate.concat(`
-      <tr ${transaction.פרטים && transaction.פרטים == '0'
-        ? 'class="bank-transaction"'
-        : ''
+      <tr ${
+        movementOrBank ? 'class="bank-transaction"' : ''
       } onClick='setSelected(this);'>
         <td>${counter++}</td>
         <td>
-          <input onchange="changeConfirmation('${transaction.id}', this${transaction.פרטים && transaction.פרטים == '0'
-        ? ", '" + transaction.חשבון_חובה_1 + "'"
-        : ''
-      });" type="checkbox" 
+          <input onchange="changeConfirmation('${transaction.id}', this${
+      movementOrBank ? ", '" + transaction.חשבון_חובה_1 + "'" : ''
+    });" type="checkbox" 
           id="${transaction.id}" ${transaction.reviewed ? 'checked' : ''}>
         </td>
         <td class="invoiceDate">
           ${transaction.תאריך_חשבונית}
-          <img download class="invoiceImage" src="${transaction.proforma_invoice_file
-      }">
+          <img download class="invoiceImage" src="${
+            transaction.proforma_invoice_file
+          }">
         </td>
         <td>${transaction.חשבון_חובה_1 ? transaction.חשבון_חובה_1 : ''}</td>
         <td>${transaction.סכום_חובה_1 ? transaction.סכום_חובה_1 : ''}</td>
-        <td>${transaction.מטח_סכום_חובה_1 ? transaction.מטח_סכום_חובה_1 : ''
-      }</td>
+        <td>${
+          transaction.מטח_סכום_חובה_1 ? transaction.מטח_סכום_חובה_1 : ''
+        }</td>
         <td>${transaction.מטבע ? transaction.מטבע : ''}</td>
         <td>${transaction.חשבון_זכות_1 ? transaction.חשבון_זכות_1 : ''}</td>
         <td>${transaction.סכום_זכות_1 ? transaction.סכום_זכות_1 : ''}</td>
-        <td>${transaction.מטח_סכום_זכות_1 ? transaction.מטח_סכום_זכות_1 : ''
-      }</td>
+        <td>${
+          transaction.מטח_סכום_זכות_1 ? transaction.מטח_סכום_זכות_1 : ''
+        }</td>
         <td>${transaction.חשבון_חובה_2 ? transaction.חשבון_חובה_2 : ''}</td>
         <td>${transaction.סכום_חובה_2 ? transaction.סכום_חובה_2 : ''}</td>
-        <td>${transaction.מטח_סכום_חובה_2 ? transaction.מטח_סכום_חובה_2 : ''
-      }</td>
+        <td>${
+          transaction.מטח_סכום_חובה_2 ? transaction.מטח_סכום_חובה_2 : ''
+        }</td>
         <td>${transaction.חשבון_זכות_2 ? transaction.חשבון_זכות_2 : ''}</td>
         <td>${transaction.סכום_זכות_2 ? transaction.סכום_זכות_2 : ''}</td>
-        <td>${transaction.מטח_סכום_זכות_2 ? transaction.מטח_סכום_זכות_2 : ''
-      }</td>
+        <td>${
+          transaction.מטח_סכום_זכות_2 ? transaction.מטח_סכום_זכות_2 : ''
+        }</td>
         <td>${transaction.פרטים ? transaction.פרטים : ''}</td>
         <td>${transaction.אסמכתא_1 ? transaction.אסמכתא_1 : ''}</td>
         <td>${transaction.אסמכתא_2 ? transaction.אסמכתא_2 : ''}</td>
@@ -186,14 +195,24 @@ export const reportToReview = async (query: any): Promise<string> => {
           </div>
         </td>
         <td>${transaction.תאריך_3 ? transaction.תאריך_3 : ''}</td>
-        <td>${transaction.hashavshevet_id ? transaction.hashavshevet_id : ''}
+        <td ${
+          missingHashavshevetSync
+            ? 'style="background-color: rgb(255,0,0);"'
+            : ''
+        }>
+          ${transaction.hashavshevet_id ? transaction.hashavshevet_id : ''}
         <button type="button"
-          ${transaction.פרטים && transaction.פרטים == '0'
-        ? generateTaxFunctionCall
-        : sendToHashavshevetFunctionCall
-      }>e
+          ${
+            movementOrBank
+              ? generateTaxFunctionCall
+              : sendToHashavshevetFunctionCall
+          }>e
         </button>
-        <button type="button" onClick='deleteTaxMovements("${transaction.id}");'>D</button>
+        ${
+          movementOrBank
+            ? ''
+            : `<button type="button" onClick='deleteTaxMovements("${transaction.id}");'>D</button>`
+        }
         </td>
       </tr>
       `);
