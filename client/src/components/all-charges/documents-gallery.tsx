769ddd3909d572aecde709ab4c57274ec35af419
TuvalSimha <tuval.simha@gmail.com>
2022-06-22 10:57:34 +0300

    fix

--- client/src/components/all-charges/documents-gallery.tsx
+++ client/src/components/all-charges/documents-gallery.tsx
@@ -82,36 +82,34 @@ export const DocumentsGallery = ({ additionalDocumentsData }: Props) => {
   const image = additionalDocumentsData[0]?.image;
 
   return (
-    <>
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-12 mx-auto">
-          <div className="flex flex-col text-center w-full mb-5">
-            {image ? <h1 className="text-lg">Realted Documents</h1> : <Badge color="red">No Documents Related</Badge>}
-          </div>
-          <div className="flex flex-wrap -m-4">
-            {additionalDocumentsData.map(doc => (
-              <>
-                <div className="p-4 md:w-2/4">
-                  <div key={doc.id} className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-                    <h2 className="text-gray-900 text-lg title-font font-medium">{doc.__typename}</h2>
-                    <Image
-                      onClick={() => setOpenModal(doc.id)}
-                      src={doc.image}
-                      className="max-w-[100%] text-gray-900 text-lg title-font font-medium"
-                    />
-                  </div>
-                </div>
-                <DocumentsPopUp
-                  onClose={() => setOpenModal(null)}
-                  key={doc.id}
-                  opened={openModal === doc.id}
-                  documentData={doc}
-                />
-              </>
-            ))}
-          </div>
+    <div className="text-gray-600 body-font">
+      <div className="container px-5 py-12 mx-auto">
+        <div className="flex flex-col text-center w-full mb-5">
+          {image ? <h1 className="text-lg">Realted Documents</h1> : <Badge color="red">No Documents Related</Badge>}
         </div>
-      </section>
-    </>
+        <div className="flex flex-wrap -m-4">
+          {additionalDocumentsData.map(doc => (
+            <>
+              <div className="p-4 md:w-2/4">
+                <div key={doc.id} className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                  <h2 className="text-gray-900 text-lg title-font font-medium">{doc.__typename}</h2>
+                  <Image
+                    onClick={() => setOpenModal(doc.id)}
+                    src={doc.image}
+                    className="max-w-[100%] text-gray-900 text-lg title-font font-medium"
+                  />
+                </div>
+              </div>
+              <DocumentsPopUp
+                onClose={() => setOpenModal(null)}
+                key={doc.id}
+                opened={openModal === doc.id}
+                documentData={doc}
+              />
+            </>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
