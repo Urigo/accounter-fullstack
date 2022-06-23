8c041a3d4c88ab092bb37c82cde3a02e086361aa
gilgardosh <gilgardosh@gmail.com>
2022-06-23 12:46:50 +0300

    add ledger update/insert hooks

---
+++ client/src/hooks/use-insert-ledger-record.tsx
@@ -0,0 +1,37 @@
+import { gql } from 'graphql-tag';
+
+import { InsertLedgerRecordMutation, InsertLedgerRecordMutationVariables, useInsertLedgerRecordMutation } from '../__generated__/types';
+
+gql`
+  mutation InsertLedgerRecord($chargeId: ID!, $record: UpdateLedgerRecordInput!) {
+    insertLedgerRecord(chargeId: $chargeId, record: $record) {
+      __typename
+      ... on Charge {
+        id
+      }
+      ... on CommonError {
+        message
+      }
+    }
+  }
+`;
+
+export const useInsertLedgerRecord = () => {
+  // TODO: add authentication
+  // TODO: add local data update method after insert
+
+  const onError = async (e: unknown, { chargeId }: InsertLedgerRecordMutationVariables) => {
+    console.log(e);
+    return new Error(`Error inserting ledger record to charge ID [${chargeId}]: ${(e as Error)?.message}`);
+  };
+  const onSuccess = async (data: InsertLedgerRecordMutation) => {
+    if (data.insertLedgerRecord.__typename === 'CommonError') {
+      throw new Error(data.insertLedgerRecord.message);
+    }
+    return data.insertLedgerRecord;
+  };
+  return useInsertLedgerRecordMutation({
+    onError,
+    onSuccess,
+  });
+};
---
+++ client/src/hooks/use-update-ledger-record.tsx
@@ -0,0 +1,37 @@
+import { gql } from 'graphql-tag';
+
+import { UpdateLedgerRecordMutation, UpdateLedgerRecordMutationVariables, useUpdateLedgerRecordMutation } from '../__generated__/types';
+
+gql`
+  mutation UpdateLedgerRecord($ledgerRecordId: ID!, $fields: UpdateLedgerRecordInput!) {
+    updateLedgerRecord(ledgerRecordId: $ledgerRecordId, fields: $fields) {
+      __typename
+      ... on LedgerRecord {
+        id
+      }
+      ... on CommonError {
+        message
+      }
+    }
+  }
+`;
+
+export const useUpdateLedgerRecord = () => {
+  // TODO: add authentication
+  // TODO: add local data update method after change
+
+  const onError = async (e: unknown, { ledgerRecordId }: UpdateLedgerRecordMutationVariables) => {
+    console.log(e);
+    return new Error(`Error updating ledger record ID [${ledgerRecordId}]: ${(e as Error)?.message}`);
+  };
+  const onSuccess = async (data: UpdateLedgerRecordMutation) => {
+    if (data.updateLedgerRecord.__typename === 'CommonError') {
+      throw new Error(data.updateLedgerRecord.message);
+    }
+    return data.updateLedgerRecord;
+  };
+  return useUpdateLedgerRecordMutation({
+    onError,
+    onSuccess,
+  });
+};
