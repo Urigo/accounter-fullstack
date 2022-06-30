77bbdc96b4b3f3d8315a2a25b3d95b885ef795d5
gilgardosh <gilgardosh@gmail.com>
2022-06-30 16:58:04 +0300

    add tags input component

--- client/src/components/common/inputs/index.ts
+++ client/src/components/common/inputs/index.ts
@@ -2,4 +2,5 @@ export * from './beneficiaries-input';
 export * from './currency-input';
 export * from './number-input';
 export * from './percentage-input';
+export * from './tags-input';
 export * from './text-input';
