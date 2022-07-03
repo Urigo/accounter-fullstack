eb9b44a36e13c144a8ed4812d59ae1280200aa7e
gilgardosh <gilgardosh@gmail.com>
2022-07-03 14:23:21 +0300

    separate input fields from edit modal info

--- client/src/components/all-charges/ledger-records/edit-ledger-record.tsx
+++ client/src/components/all-charges/ledger-records/edit-ledger-record.tsx
@@ -1,11 +1,10 @@
 import gql from 'graphql-tag';
-import moment from 'moment';
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { EditLedgerRecordsFieldsFragment, UpdateLedgerRecordInput } from '../../../__generated__/types';
 import { MakeBoolean, relevantDataPicker } from '../../../helpers';
 import { useUpdateLedgerRecord } from '../../../hooks/use-update-ledger-record';
-import { CurrencyInput, NumberInput, TextInput } from '../../common/inputs';
+import { LedgerRecordFields } from './ledger-record-fields';
 
 gql`
   fragment EditLedgerRecordsFields on LedgerRecord {
@@ -41,7 +40,7 @@ export const EditLedgerRecord = ({ ledgerRecord, onAccept, onCancel }: Props) =>
     control,
     handleSubmit,
     formState: { dirtyFields },
-  } = useForm<UpdateLedgerRecordInput>();
+  } = useForm<UpdateLedgerRecordInput>({ defaultValues: ledgerRecord });
   const { mutate, isLoading } = useUpdateLedgerRecord();
 
   const onSubmit: SubmitHandler<UpdateLedgerRecordInput> = data => {
@@ -67,135 +66,7 @@ export const EditLedgerRecord = ({ ledgerRecord, onAccept, onCancel }: Props) =>
           <p>ID: {ledgerRecord.id}</p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-              <Controller
-                name="creditAccount.name"
-                control={control}
-                defaultValue={ledgerRecord.creditAccount?.name}
-                rules={{ required: 'Required' }}
-                render={({ field, fieldState }) => (
-                  <TextInput {...field} error={fieldState.error?.message} label="Credit Account" />
-                )}
-              />
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-              <Controller
-                name="debitAccount.name"
-                control={control}
-                defaultValue={ledgerRecord.debitAccount?.name}
-                rules={{ required: 'Required' }}
-                render={({ field, fieldState }) => (
-                  <TextInput {...field} error={fieldState.error?.message} label="Debit Account" />
-                )}
-              />
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-              <Controller
-                name="date"
-                control={control}
-                defaultValue={ledgerRecord.date}
-                rules={{ required: 'Required' }}
-                render={({ field: { value, ...fieldProps }, fieldState }) => {
-                  // TODO: update after adding DATE scalar (for timeless dates)
-                  const parsedDate = moment(value).format('YYYY-MM-DD');
-                  return (
-                    <TextInput {...fieldProps} value={parsedDate} error={fieldState.error?.message} label="Date" />
-                  );
-                }}
-              />
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-              <Controller
-                name="description"
-                control={control}
-                defaultValue={ledgerRecord.description}
-                render={({ field, fieldState }) => (
-                  <TextInput
-                    {...field}
-                    value={!field || field.value === 'Missing' ? '' : field.value!}
-                    error={fieldState.error?.message}
-                    label="Description"
-                  />
-                )}
-              />
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-              <Controller
-                name="localCurrencyAmount.raw"
-                control={control}
-                defaultValue={ledgerRecord.localCurrencyAmount.raw}
-                render={({ field: amountField, fieldState: amountFieldState }) => (
-                  <Controller
-                    name="localCurrencyAmount.currency"
-                    control={control}
-                    defaultValue={ledgerRecord.localCurrencyAmount.currency}
-                    render={({ field: currencyCodeField, fieldState: currencyCodeFieldState }) => (
-                      <CurrencyInput
-                        {...amountField}
-                        error={amountFieldState.error?.message || currencyCodeFieldState.error?.message}
-                        label="Local Currency"
-                        currencyCodeProps={{ ...currencyCodeField, label: 'Currency', disabled: true }}
-                      />
-                    )}
-                  />
-                )}
-              />
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-              <Controller
-                name="originalAmount.raw"
-                control={control}
-                defaultValue={ledgerRecord.originalAmount.raw}
-                render={({ field: amountField, fieldState: amountFieldState }) => (
-                  <Controller
-                    name="originalAmount.currency"
-                    control={control}
-                    defaultValue={ledgerRecord.originalAmount.currency}
-                    render={({ field: currencyCodeField, fieldState: currencyCodeFieldState }) => (
-                      <CurrencyInput
-                        {...amountField}
-                        error={amountFieldState.error?.message || currencyCodeFieldState.error?.message}
-                        label="Original Currency"
-                        currencyCodeProps={{ ...currencyCodeField, label: 'Currency' }}
-                      />
-                    )}
-                  />
-                )}
-              />
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-              <Controller
-                name="hashavshevetId"
-                control={control}
-                defaultValue={ledgerRecord.hashavshevetId}
-                render={({ field: { value, ...field }, fieldState }) => {
-                  const adjustedValue = value ?? undefined;
-                  return (
-                    <NumberInput
-                      precision={0}
-                      value={adjustedValue}
-                      {...field}
-                      error={fieldState.error?.message}
-                      label="Hashavshevet ID"
-                    />
-                  );
-                }}
-              />
-            </div>
-          </div>
+          <LedgerRecordFields ledgerRecord={ledgerRecord} control={control} />
         </div>
         <div className="container flex justify-center gap-20">
           <button
