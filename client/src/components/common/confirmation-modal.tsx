e6c50cb8c5841e71b8336f99c8e14708c0285cab
gilgardosh <gilgardosh@gmail.com>
2022-07-07 12:57:24 +0300

    create common confirmation modal

---
+++ client/src/components/common/confirmation-modal.tsx
@@ -0,0 +1,40 @@
+import { Modal, ModalProps } from '@mantine/core';
+import { PropsWithChildren } from 'react';
+
+interface Props extends ModalProps {
+  labels?: { cancel?: string; confirm?: string };
+  onConfirm: () => void;
+}
+
+export function ConfirmationModal({
+  labels = { cancel: 'Cancel', confirm: 'Confirm' },
+  title,
+  onConfirm,
+  children,
+  ...props
+}: PropsWithChildren<Props>) {
+  return (
+    <Modal closeOnEscape withCloseButton={false} {...props}>
+      <div className="flex flex-wrap -mx-4 -mb-10 text-center">
+        <div className="mb-10 px-4">
+          {title && <h3 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">{title}</h3>}
+          {children}
+          <div className="flex mx-auto flex-row justify-evenly">
+            <button
+              onClick={props.onClose}
+              className="flex mt-6 text-white bg-red-500 border-0 py-2 px-5 focus:outline-none hover:bg-red-600 rounded"
+            >
+              {labels.cancel}
+            </button>
+            <button
+              onClick={onConfirm}
+              className="flex mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded"
+            >
+              {labels.confirm}
+            </button>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+}
