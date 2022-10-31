eee7ffc3be694a8ce405aba00b490cff6f0e7137
gilgardosh <gilgardosh@gmail.com>
2022-10-31 13:08:55 +0200

    vat accept float fix

--- client/src/components/common/inputs/number-input.tsx
+++ client/src/components/common/inputs/number-input.tsx
@@ -5,5 +5,5 @@ import { Input } from './input';
 type Props = PropsWithChildren<Omit<ComponentProps<typeof Input>, 'type'>>;
 
 export const NumberInput = forwardRef<HTMLInputElement, Props>(function NumberInput(props) {
-  return <Input {...props} type="number" />;
+  return <Input {...props} type="number" step="any" />;
 });
