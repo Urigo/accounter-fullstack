9a8d91e3d2343011e4b92ba6e432eecc59d3a0cd
gilgardosh <gilgardosh@gmail.com>
2023-02-08 00:31:48 +0200

    migrate match-document modal to common

--- client/src/components/all-charges/index.tsx
+++ client/src/components/all-charges/index.tsx
@@ -10,11 +10,10 @@ import {
   EditChargeModal,
   InsertDocumentModal,
   InsertLedgerRecordModal,
+  MatchDocumentModal,
   NavBar,
-  PopUpDrawer,
   UploadDocumentModal,
 } from '../common';
-import { DocumentsToChargeMatcher } from '../documents-to-charge-matcher';
 import { AllChargesTable } from './all-charges-table';
 import { ChargesFilters } from './charges-filters';
 
@@ -169,25 +168,7 @@ export const AllCharges = () => {
         />
       )}
       {matchDocuments && (
-        <PopUpDrawer
-          modalSize="80%"
-          position="bottom"
-          title={
-            <div className="flex flex-row mx-3 pt-3 sm:text-1xl gap-5">
-              <h1 className="sm:text-2xl font-small text-gray-900">Match Documents:</h1>
-              <a href="/#" className="pt-1">
-                Charge ID: {matchDocuments}
-              </a>
-            </div>
-          }
-          opened={Boolean(matchDocuments)}
-          onClose={() => setMatchDocuments(undefined)}
-        >
-          <DocumentsToChargeMatcher
-            chargeId={matchDocuments}
-            onDone={() => setMatchDocuments(undefined)}
-          />
-        </PopUpDrawer>
+        <MatchDocumentModal matchDocuments={matchDocuments} setMatchDocuments={setMatchDocuments} />
       )}
     </div>
   );
---
+++ client/src/components/common/modals/match-document-modal.tsx
@@ -0,0 +1,31 @@
+import { DocumentsToChargeMatcher } from '../../documents-to-charge-matcher';
+import { PopUpDrawer } from '..';
+
+interface Props {
+  matchDocuments: string;
+  setMatchDocuments: React.Dispatch<React.SetStateAction<string | undefined>>;
+}
+
+export const MatchDocumentModal = ({ matchDocuments, setMatchDocuments }: Props) => {
+  return (
+    <PopUpDrawer
+      modalSize="80%"
+      position="bottom"
+      title={
+        <div className="flex flex-row mx-3 pt-3 sm:text-1xl gap-5">
+          <h1 className="sm:text-2xl font-small text-gray-900">Match Documents:</h1>
+          <a href="/#" className="pt-1">
+            Charge ID: {matchDocuments}
+          </a>
+        </div>
+      }
+      opened={Boolean(matchDocuments)}
+      onClose={() => setMatchDocuments(undefined)}
+    >
+      <DocumentsToChargeMatcher
+        chargeId={matchDocuments}
+        onDone={() => setMatchDocuments(undefined)}
+      />
+    </PopUpDrawer>
+  );
+};
