6dc6bde55a0f1c3fc87bb98e3db24b130922dded
gilgardosh <gilgardosh@gmail.com>
2023-02-12 16:43:03 +0200

    lint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -46,7 +46,7 @@ module.exports = {
                 'VatReportResult',
                 'VatReportRecord',
                 'ValidationData',
-                'GeneratePCNResult',
+                'PCNFileResult',
                 'PCNRawData',
               ],
             },
