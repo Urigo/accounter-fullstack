d782ffc05b462481f42fb153b72551f03c18e130
gilgardosh <gilgardosh@gmail.com>
2023-03-22 18:36:32 +0200

    make charge's entity clickable

--- client/src/components/all-charges/cells/entity.tsx
+++ client/src/components/all-charges/cells/entity.tsx
@@ -1,9 +1,14 @@
 import { useCallback } from 'react';
-import { Indicator } from '@mantine/core';
+import { Indicator, NavLink } from '@mantine/core';
 import { FragmentType, getFragmentData } from '../../../gql';
-import { AllChargesEntityFieldsFragmentDoc, MissingChargeInfo } from '../../../gql/graphql';
+import {
+  AllChargesEntityFieldsFragmentDoc,
+  ChargeFilter,
+  MissingChargeInfo,
+} from '../../../gql/graphql';
 import type { SuggestedCharge } from '../../../helpers';
 import { useUpdateCharge } from '../../../hooks/use-update-charge';
+import { useUrlQuery } from '../../../hooks/use-url-query';
 import { ConfirmMiniButton } from '../../common';
 
 /* GraphQL */ `
@@ -11,6 +16,7 @@ import { ConfirmMiniButton } from '../../common';
     id
     counterparty {
       name
+      id
     }
     validationData {
       missingInfo
@@ -24,6 +30,7 @@ type Props = {
 };
 
 export const Entity = ({ data, alternativeCharge }: Props) => {
+  const { get } = useUrlQuery();
   const {
     counterparty,
     id: chargeId,
@@ -31,7 +38,7 @@ export const Entity = ({ data, alternativeCharge }: Props) => {
   } = getFragmentData(AllChargesEntityFieldsFragmentDoc, data);
   const isError = validationData?.missingInfo?.includes(MissingChargeInfo.Counterparty);
   const hasAlternative = isError && !!alternativeCharge?.financialEntity.trim()?.length;
-  const { name } = counterparty || {};
+  const { name, id } = counterparty || {};
   const alternativeName = hasAlternative ? alternativeCharge.financialEntity.trim() : 'Missing';
   const cellText = isError ? alternativeName : name;
 
@@ -55,11 +62,48 @@ export const Entity = ({ data, alternativeCharge }: Props) => {
     [chargeId, updateCharge],
   );
 
+  const encodedFilters = get('chargesFilters');
+
+  const getHref = useCallback(
+    (businessID: string) => {
+      const currentFilters = encodedFilters
+        ? (JSON.parse(decodeURIComponent(encodedFilters as string)) as ChargeFilter)
+        : {};
+      const encodedNewFilters = {
+        fromDate: currentFilters.fromDate
+          ? `%252C%2522fromDate%2522%253A%2522${currentFilters.fromDate}%2522`
+          : '',
+        toDate: currentFilters.toDate
+          ? `%252C%2522toDate%2522%253A%2522${currentFilters.toDate}%2522`
+          : '',
+        financialEntityIds:
+          currentFilters.byOwners && currentFilters.byOwners.length > 0
+            ? `%2522${currentFilters.byOwners.join('%2522%252C%2522')}%2522`
+            : '',
+      };
+      return `/business-transactions?transactionsFilters=%257B%2522financialEntityIds%2522%253A%255B${
+        encodedNewFilters.financialEntityIds
+      }%255D%252C%2522businessIDs%2522%253A%255B%2522${encodeURIComponent(businessID)}%2522%255D${
+        encodedNewFilters.fromDate
+      }${encodedNewFilters.toDate}%257D`;
+    },
+    [encodedFilters],
+  );
+
+  const content = (
+    <p style={hasAlternative ? { backgroundColor: 'rgb(236, 207, 57)' } : {}}>{cellText}</p>
+  );
+
   return (
     <td>
       <div className="flex flex-wrap">
         <Indicator inline size={12} disabled={!isError} color="red" zIndex="auto">
-          <p style={hasAlternative ? { backgroundColor: 'rgb(236, 207, 57)' } : {}}>{cellText}</p>
+          {!isError && (
+            <a href={getHref(id)} target="_blank" rel="noreferrer">
+              <NavLink label={content} className="[&>*>.mantine-NavLink-label]:font-semibold" />
+            </a>
+          )}
+          {isError && content}
         </Indicator>
         {hasAlternative && (
           <ConfirmMiniButton onClick={() => updateTag(cellText)} disabled={fetching} />
