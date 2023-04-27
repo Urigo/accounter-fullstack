cb391ed80a939c93ece44072041453eaded0d8ea
gilgardosh <gilgardosh@gmail.com>
2023-04-27 18:29:27 +0300

    update edit charge form

--- client/src/components/common/forms/edit-charge.tsx
+++ client/src/components/common/forms/edit-charge.tsx
@@ -4,20 +4,18 @@ import { useQuery } from 'urql';
 import { Select, Switch } from '@mantine/core';
 import { showNotification } from '@mantine/notifications';
 import {
-  // BeneficiariesInput, CurrencyInput,
+  // BeneficiariesInput,
   SimpleGrid, // TagsInput,
   TextInput,
 } from '..';
 import { FragmentType, getFragmentData } from '../../../gql';
 import {
-  AllFinancialEntitiesDocument, // Currency,
+  AllFinancialEntitiesDocument,
   EditChargeFieldsFragmentDoc,
-  UpdateChargeInput, // UpdateTransactionInput,
+  UpdateChargeInput,
 } from '../../../gql/graphql';
 import { MakeBoolean, relevantDataPicker } from '../../../helpers';
 import { useUpdateCharge } from '../../../hooks/use-update-charge';
-
-// import { useUpdateTransaction } from '../../../hooks/use-update-transaction';
 
 /* GraphQL */ `
   fragment EditChargeFields on Charge {
@@ -56,20 +54,10 @@ export const EditCharge = ({ chargeProps, onAccept, onCancel }: Props) => {
     formState: { dirtyFields: dirtyChargeFields },
   } = useFormManager;
 
-  // const transaction = charge.transactions[0];
-
-  // const {
-  //   control: transactionControl,
-  //   handleSubmit: handleTransactionSubmit,
-  //   formState: { dirtyFields: dirtyTransactionFields },
-  // } = useForm<UpdateTransactionInput>({ defaultValues: transaction });
-
   const { updateCharge, fetching: isChargeLoading } = useUpdateCharge();
-  // const { updateTransaction, fetching: isTransactionLoading } = useUpdateTransaction();
 
   const onChargeSubmit: SubmitHandler<UpdateChargeInput> = data => {
     const dataToUpdate = relevantDataPicker(data, dirtyChargeFields as MakeBoolean<typeof data>);
-    // handleTransactionSubmit(onTransactionSubmit)();
     if (onAccept) {
       onAccept();
     }
@@ -80,22 +68,6 @@ export const EditCharge = ({ chargeProps, onAccept, onCancel }: Props) => {
       });
     }
   };
-
-  // const onTransactionSubmit: SubmitHandler<UpdateTransactionInput> = data => {
-  //   const dataToUpdate = relevantDataPicker(
-  //     data,
-  //     dirtyTransactionFields as MakeBoolean<typeof data>,
-  //   );
-  //   if (dataToUpdate && Object.keys(dataToUpdate).length > 0) {
-  //     updateTransaction({
-  //       transactionId: transaction.id,
-  //       fields: dataToUpdate,
-  //     });
-  //   }
-  //   if (onAccept) {
-  //     onAccept();
-  //   }
-  // };
 
   const [{ data, fetching, error: financialEntitiesError }] = useQuery({
     query: AllFinancialEntitiesDocument,
@@ -146,28 +118,6 @@ export const EditCharge = ({ chargeProps, onAccept, onCancel }: Props) => {
             )}
           />
           <Controller
-            name="counterpartyId"
-            control={chargeControl}
-            defaultValue={charge.counterparty?.id}
-            rules={{
-              required: 'Required',
-              minLength: { value: 2, message: 'Minimum 2 characters' },
-            }}
-            render={({ field, fieldState }) => (
-              <Select
-                {...field}
-                data={financialEntities}
-                value={field.value}
-                disabled={fetching}
-                label="Counterparty"
-                placeholder="Scroll to see all options"
-                maxDropdownHeight={160}
-                searchable
-                error={fieldState.error?.message}
-              />
-            )}
-          />
-          <Controller
             name="ownerId"
             control={chargeControl}
             defaultValue={charge.owner?.id}
@@ -189,48 +139,6 @@ export const EditCharge = ({ chargeProps, onAccept, onCancel }: Props) => {
               />
             )}
           />
-          {/* <Controller
-            name="totalAmount.raw"
-            control={chargeControl}
-            defaultValue={charge.totalAmount?.raw}
-            render={({ field: amountField, fieldState: amountFieldState }) => (
-              <Controller
-                name="totalAmount.currency"
-                control={chargeControl}
-                defaultValue={charge.totalAmount?.currency ?? Currency.Ils}
-                render={({ field: currencyCodeField, fieldState: currencyCodeFieldState }) => (
-                  <CurrencyInput
-                    {...amountField}
-                    value={amountField.value ?? undefined}
-                    error={amountFieldState.error?.message || currencyCodeFieldState.error?.message}
-                    label="Total Amount"
-                    currencyCodeProps={{ ...currencyCodeField, label: 'Currency' }}
-                  />
-                )}
-              />
-            )}
-          />
-          <Controller
-            name="vat"
-            control={chargeControl}
-            defaultValue={charge.vat?.raw}
-            render={({ field: amountField, fieldState: amountFieldState }) => (
-              <Controller
-                name="totalAmount.currency"
-                control={chargeControl}
-                defaultValue={charge.totalAmount?.currency ?? Currency.Ils}
-                render={({ field: currencyCodeField, fieldState: currencyCodeFieldState }) => (
-                  <CurrencyInput
-                    {...amountField}
-                    value={amountField.value ?? undefined}
-                    error={amountFieldState.error?.message || currencyCodeFieldState.error?.message}
-                    label="Vat"
-                    currencyCodeProps={{ ...currencyCodeField, label: 'Currency', disabled: true }}
-                  />
-                )}
-              />
-            )}
-          /> */}
           <Controller
             name="isProperty"
             control={chargeControl}
@@ -242,7 +150,7 @@ export const EditCharge = ({ chargeProps, onAccept, onCancel }: Props) => {
           <Controller
             name="isConversion"
             control={chargeControl}
-            defaultValue={charge.property}
+            defaultValue={charge.conversion}
             render={({ field: { value, ...field } }) => {
               return <Switch {...field} checked={value === true} label="Is Conversion" />;
             }}
@@ -256,12 +164,7 @@ export const EditCharge = ({ chargeProps, onAccept, onCancel }: Props) => {
           type="submit"
           onClick={() => handleChargeSubmit(onChargeSubmit)}
           className="mt-8 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-          disabled={
-            isChargeLoading ||
-            // isTransactionLoading ||
-            Object.keys(dirtyChargeFields).length === 0
-            //  + Object.keys(dirtyTransactionFields).length
-          }
+          disabled={isChargeLoading || Object.keys(dirtyChargeFields).length === 0}
         >
           Accept
         </button>
