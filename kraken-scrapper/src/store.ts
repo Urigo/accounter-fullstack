019d9e937e02326d97c9ff0d9099b69bed5aec96
Dotan Simha <dotansimha@gmail.com>
2023-05-22 12:46:28 +0300

    Kraken scraper fixes (#291)

--- kraken-scrapper/src/store.ts
+++ kraken-scrapper/src/store.ts
@@ -151,7 +151,7 @@ export async function createAndConnectStore(options: { connectionString: string;
           trade_id TEXT PRIMARY KEY,
           account_nickname TEXT NOT NULL,
           pair TEXT NOT NULL,
-          value_date DATE NOT NULL,
+          value_date TIMESTAMP NOT NULL,
           order_type TEXT NOT NULL,
           price NUMERIC NOT NULL,
           cost NUMERIC NOT NULL,
@@ -175,7 +175,7 @@ export async function createAndConnectStore(options: { connectionString: string;
           amount DECIMAL NOT NULL,
           balance DECIMAL NOT NULL,
           fee DECIMAL NOT NULL,
-          value_date DATE NOT NULL,
+          value_date TIMESTAMP NOT NULL,
           trade_ref_id TEXT,
           raw_data JSONB NOT NULL
         );
