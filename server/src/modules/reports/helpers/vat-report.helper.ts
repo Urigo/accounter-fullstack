db52472b0a09df6487c265b66c7603e223722579
gilgardosh <gilgardosh@gmail.com>
2023-07-17 21:30:24 +0300

    PCN report refactor

--- server/src/modules/reports/helpers/vat-report.helper.ts
+++ server/src/modules/reports/helpers/vat-report.helper.ts
@@ -1,5 +1,6 @@
 import type { IGetChargesByFiltersResult } from '@modules/charges/types';
 import type { IGetDocumentsByFiltersResult } from '@modules/documents/types';
+import { IGetFinancialEntitiesByIdsResult } from '@modules/financial-entities/types';
 import {
   getClosestRateForDate,
   getRateForCurrency,
@@ -7,59 +8,10 @@ import {
 import type { IGetExchangeRatesByDatesResult } from '@modules/ledger/types.js';
 import { TAX_CATEGORIES_WITH_NOT_FULL_VAT } from '@shared/constants';
 
-// export function mergeChargeDoc(
-//   charge: ChargesTypes.IGetChargesByIdsResult,
-//   doc: DocumentsTypes.IGetDocumentsByFiltersResult,
-//   business?: IGetFinancialEntitiesByIdsResult,
-// ) {
-//   return {
-//     tax_invoice_date: doc.date,
-//     tax_invoice_number: doc.serial_number,
-//     tax_invoice_amount: doc.total_amount,
-//     currency_code: doc.currency_code,
-//     document_image_url: doc.image_url,
-//     document_id: doc.id,
-//     tax_category: charge.tax_category,
-//     event_date: charge.event_date,
-//     debit_date: charge.debit_date,
-//     event_amount: charge.event_amount,
-//     financial_entity_id: charge.financial_entity_id,
-//     vat: charge.vat,
-//     user_description: charge.user_description,
-//     bank_description: charge.bank_description,
-//     withholding_tax: charge.withholding_tax,
-//     interest: charge.interest,
-//     id: charge.id,
-//     detailed_bank_description: charge.detailed_bank_description,
-//     receipt_number: charge.receipt_number,
-//     business_trip: charge.business_trip,
-//     personal_category: charge.personal_category,
-//     financial_accounts_to_balance: charge.financial_accounts_to_balance,
-//     bank_reference: charge.bank_reference,
-//     event_number: charge.event_number,
-//     account_number: charge.account_number,
-//     account_type: charge.account_type,
-//     is_conversion: charge.is_conversion,
-//     currency_rate: charge.currency_rate,
-//     contra_currency_code: charge.contra_currency_code,
-//     original_id: charge.original_id,
-//     reviewed: charge.reviewed,
-//     hashavshevet_id: charge.hashavshevet_id,
-//     current_balance: charge.current_balance,
-//     tax_invoice_file: charge.tax_invoice_file,
-//     links: charge.links,
-//     receipt_image: charge.receipt_image,
-//     receipt_url: charge.receipt_url,
-//     receipt_date: charge.receipt_date,
-//     is_property: charge.is_property,
-//     tax_invoice_currency: charge.tax_invoice_currency,
-//     vat_number: business?.vat_number,
-//   };
-// }
-
 export type RawVatReportRecord = {
   charge: IGetChargesByFiltersResult;
   doc: IGetDocumentsByFiltersResult;
+  business: IGetFinancialEntitiesByIdsResult;
 };
 export type DecoratedVatReportRecord = RawVatReportRecord & {
   currency_code: string;
@@ -89,6 +41,9 @@ export type RawVatReportRecord2 = {
   chargeDate: Date;
   documentDate: Date | null;
   documentSerial: string | null;
+  isProperty: boolean;
+  vatNumber?: string | null;
+  isExpense: boolean;
 };
 
 export function adjustTaxRecords(
@@ -98,7 +53,7 @@ export function adjustTaxRecords(
   const records: RawVatReportRecord2[] = [];
 
   for (const rawRecord of rawRecords) {
-    const { charge, doc } = rawRecord;
+    const { charge, doc, business } = rawRecord;
 
     if (!doc.total_amount) {
       throw new Error(`Amount missing for invoice ID=${doc.id}`);
@@ -112,7 +67,6 @@ export function adjustTaxRecords(
     }
 
     const partialRecord: RawVatReportRecord2 = {
-      // amountBeforeVAT
       businessId: charge.business_id,
       chargeDate: charge.transactions_min_event_date ?? charge.documents_min_date!, // must have min_date, as will throw if local doc is missing date
       chargeId: charge.id,
@@ -122,11 +76,10 @@ export function adjustTaxRecords(
       documentSerial: doc.serial_number,
       documentUrl: doc.image_url,
       documentAmount: String(doc.total_amount),
-      // eventAmountILS
-      // roundedVATToAdd
       vat: doc.vat_amount,
-      // vatAfterDeduction
-      // vatAfterDeductionILS
+      isProperty: charge.is_property,
+      vatNumber: business.vat_number,
+      isExpense: doc.debtor_id === charge.owner_id,
     };
 
     // get exchange rate
--- server/src/modules/reports/resolvers/reports.resolver.ts
+++ server/src/modules/reports/resolvers/reports.resolver.ts
@@ -1,207 +1,39 @@
 import { format } from 'date-fns';
-import { GraphQLError } from 'graphql';
-import { validateCharge } from '@modules/charges/helpers/validate.helper.js';
-import { ChargesProvider } from '@modules/charges/providers/charges.provider.js';
-import { IGetChargesByFiltersResult } from '@modules/charges/types.js';
-import { DocumentsProvider } from '@modules/documents/providers/documents.provider.js';
 import { FinancialEntitiesProvider } from '@modules/financial-entities/providers/financial-entities.provider.js';
-import { ExchangeProvider } from '@modules/ledger/providers/exchange.provider.js';
 import { Currency } from '@shared/enums';
-import type { ResolversTypes } from '@shared/gql-types';
 import { formatFinancialAmount, formatFinancialIntAmount } from '@shared/helpers';
 import type { TimelessDateString } from '@shared/types';
-import { filterOutExcludedBusinesses } from '../helpers/get-vat-records.helper.js';
-import { adjustTaxRecords, RawVatReportRecord } from '../helpers/vat-report.helper.js';
-// import { generatePcnFromCharges } from '../helpers/pcn.helper.js';
+import { generatePcnFromCharges } from '../helpers/pcn.helper.js';
+import { RawVatReportRecord2 } from '../helpers/vat-report.helper.js';
 import type { ReportsModule } from '../types.js';
+import { getVatRecords } from './get-vat-records.resolver.js';
 
 export const reportsResolvers: ReportsModule.Resolvers = {
   Query: {
-    vatReport: async (_, { filters }, { injector }) => {
-      try {
-        const response = {
-          income: [] as Array<ResolversTypes['VatReportRecord']>,
-          expenses: [] as Array<ResolversTypes['VatReportRecord']>,
-          missingInfo: [] as Array<ResolversTypes['Charge']>,
-          differentMonthDoc: [] as Array<ResolversTypes['Charge']>,
-        };
-
-        // get all documents by date filters
-        const documents = await injector.get(DocumentsProvider).getDocumentsByFilters({
-          fromDate: filters?.fromDate,
-          toDate: filters?.toDate,
-          ownerIDs: [filters?.financialEntityId],
-        });
-
-        const notIncludedChargeIDs = new Set<string>();
-
-        // filter invoice documents with linked charge
-        const relevantDocuments = documents.filter(doc => {
-          if (!doc.charge_id_new) {
-            return false;
-          }
-          const isRelevantDoc = ['INVOICE', 'INVOICE_RECEIPT'].includes(doc.type);
-          if (!isRelevantDoc) {
-            notIncludedChargeIDs.add(doc.charge_id_new);
-          }
-          return isRelevantDoc;
-        });
-
-        // TODO: what if no docs found?
-
-        relevantDocuments.map(doc => {
-          if (doc.charge_id_new) notIncludedChargeIDs.delete(doc.charge_id_new);
-        });
-
-        // get all charges by IDs from documents
-        const docsChargesIDs = new Set(documents.map(doc => doc.charge_id_new as string));
-
-        const docsCharges = await injector
-          .get(ChargesProvider)
-          .getChargesByFilters({
-            IDs: Array.from(docsChargesIDs),
-            ownerIds: [filters?.financialEntityId],
-          })
-          .then(res => filterOutExcludedBusinesses(res));
-
-        const incomeRecords: Array<RawVatReportRecord> = [];
-        const expenseRecords: Array<RawVatReportRecord> = [];
-        const includedChargeIDs = new Set<string>();
-
-        await Promise.all(
-          relevantDocuments.map(async doc => {
-            // filter charge linked to document
-            const charge = docsCharges.find(charge => charge.id === doc.charge_id_new);
-            if (!charge) {
-              throw new GraphQLError(
-                `for some weird reason no charge found for document ID=${doc.id}`,
-              );
-            }
-
-            // // update tax category according to Hashavshevet
-            // TODO(Gil): how to handle this on new architecture?
-            // if (ownerId && charge.business_id) {
-            //   const hashIndex = await injector
-            //     .get(HashavshevetProvider)
-            //     .getHashavshevetBusinessIndexesByOwnerAndBusinessIDLoader.load({
-            //       financialEntityId: ownerId,
-            //       businessID: charge.business_id,
-            //     });
-            //   charge.tax_category = hashIndex?.auto_tax_category ?? charge.tax_category;
-            // }
-
-            // add charge to income/expense records
-            if (doc.vat_amount && doc.debtor_id === filters?.financialEntityId) {
-              includedChargeIDs.add(charge.id);
-              if (filters?.chargesType !== 'EXPENSE') {
-                expenseRecords.push({ charge, doc });
-              }
-            } else if (
-              doc.vat_amount != null &&
-              doc.creditor_id === filters?.financialEntityId &&
-              Number(doc.total_amount) > 0
-            ) {
-              includedChargeIDs.add(charge.id);
-              if (filters?.chargesType !== 'INCOME') {
-                incomeRecords.push({ charge, doc });
-              }
-            } else {
-              notIncludedChargeIDs.add(charge.id);
-            }
-          }),
-        );
-
-        if (incomeRecords.length + expenseRecords.length > 0) {
-          // get exchange rates for all docs dates
-          const exchangeDates = new Set<number>();
-          for (const record of [...incomeRecords, ...expenseRecords]) {
-            if (record.doc.date) {
-              exchangeDates.add(record.doc.date.getTime());
-            }
-          }
-          // TODO: what if no exchange dates found?
-          const fromDate = format(new Date(Math.min(...exchangeDates)), 'yyyy-MM-dd');
-          const toDate = format(new Date(Math.max(...exchangeDates)), 'yyyy-MM-dd');
-          const exchangeRates = await injector
-            .get(ExchangeProvider)
-            .getExchangeRatesByDates({ fromDate, toDate });
-
-          response.income.push(...adjustTaxRecords(incomeRecords, exchangeRates));
-          response.expenses.push(...adjustTaxRecords(expenseRecords, exchangeRates));
-        }
-
-        const charges = await injector.get(ChargesProvider).getChargesByFilters({
-          fromDate: filters?.fromDate,
-          toDate: filters?.toDate,
-          ownerIds: filters?.financialEntityId ? [filters?.financialEntityId] : undefined,
-          chargeType: filters?.chargesType,
-        });
-        charges.map(c => {
-          notIncludedChargeIDs.delete(c.id);
-        });
-        if (notIncludedChargeIDs.size > 0) {
-          const moreCharges = await injector.get(ChargesProvider).getChargesByFilters({
-            IDs: Array.from(notIncludedChargeIDs),
-            ownerIds: filters?.financialEntityId ? [filters?.financialEntityId] : undefined,
-            chargeType: filters?.chargesType,
-          });
-          charges.push(...moreCharges);
-        }
-
-        // validate charges for missing info
-        const validatedCharges = await Promise.all<{
-          charge: IGetChargesByFiltersResult;
-          isValid: boolean;
-        }>(
-          charges.map(
-            c =>
-              new Promise((resolve, reject) => {
-                validateCharge(c, injector)
-                  .then(res => {
-                    if ('isValid' in res) {
-                      resolve({ charge: c, isValid: res.isValid });
-                    } else {
-                      reject('Error validating charge');
-                    }
-                  })
-                  .catch(reject);
-              }),
-          ),
-        );
-
-        for (const { charge, isValid } of validatedCharges) {
-          if (isValid) {
-            if (!includedChargeIDs.has(charge.id)) {
-              // If valid but not yet included, add to charges with different month doc
-              response.differentMonthDoc.push(charge);
-            }
-          } else {
-            // add to charges with missing info
-            response.missingInfo.push(charge);
-          }
-        }
-
-        return response;
-      } catch (e) {
-        console.error('Error fetching vat report records:', e);
-        throw new GraphQLError((e as Error)?.message ?? 'Error fetching vat report records');
+    vatReport: getVatRecords,
+    pcnFile: async (_, { fromDate, toDate, financialEntityId }, context, __) => {
+      const financialEntity = await context.injector
+        .get(FinancialEntitiesProvider)
+        .getFinancialEntityByIdLoader.load(financialEntityId);
+      if (!financialEntity?.vat_number) {
+        throw new Error(`Financial entity ${financialEntityId} has no VAT number`);
       }
+      const vatRecords = await getVatRecords(
+        _,
+        { filters: { fromDate, toDate, financialEntityId } },
+        context,
+        __,
+      );
+      const reportMonth = format(new Date(fromDate), 'yyyyMM');
+      return generatePcnFromCharges(
+        [
+          ...(vatRecords.income as RawVatReportRecord2[]),
+          ...(vatRecords.expenses as RawVatReportRecord2[]),
+        ],
+        financialEntity.vat_number,
+        reportMonth,
+      );
     },
-    //   pcnFile: async (_, { fromDate, toDate, financialEntityId }, { injector }) => {
-    //     const financialEntity = await injector
-    //       .get(FinancialEntitiesProvider)
-    //       .getFinancialEntityByIdLoader.load(financialEntityId);
-    //     if (!financialEntity?.vat_number) {
-    //       throw new Error(`Financial entity ${financialEntityId} has no VAT number`);
-    //     }
-    //     const vatRecords = await getVatRecords(injector, fromDate, toDate, financialEntityId, true);
-    //     const reportMonth = format(new Date(fromDate), 'yyyyMM');
-    //     return generatePcnFromCharges(
-    //       [...vatRecords.income, ...vatRecords.expenses],
-    //       financialEntity.vat_number,
-    //       reportMonth,
-    //     );
-    //   },
   },
   VatReportRecord: {
     documentId: raw => raw.documentId,
