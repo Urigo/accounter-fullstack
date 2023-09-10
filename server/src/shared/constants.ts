2016e6f3fab84c0b1ca7c01da9031e5d0d01c0bb
gilgardosh <gilgardosh@gmail.com>
2023-09-10 14:48:21 +0300

    Conversion ledger generation enhancement

--- server/src/shared/constants.ts
+++ server/src/shared/constants.ts
@@ -1,3 +1,5 @@
+import { Currency } from './enums.js';
+
 /* regex of yyyy-mm-dd  */
 export const TIMELESS_DATE_REGEX =
   /^((?:1[6-9]|[2]\d)\d{2})(-)(?:((?:0[13578]|1[02])(-31))|(?:(?:0[1,3-9]|1[0-2])(-)(?:29|30)))$|^(?:(?:(?:1[6-9]|[2]\d)(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))(-02-29)$|^(?:(?:1[6-9]|[2-9]\d)\d{2})(-)(?:(?:0[1-9])|(?:1[0-2]))(-)(?:0[1-9]|1\d|2[0-8])$/;
@@ -62,3 +64,4 @@ export const ENTITIES_WITHOUT_ACCOUNTING = [
 // Tax category related
 export const VAT_TAX_CATEGORY_NAME = 'מעמחוז';
 export const EXCHANGE_RATE_CATEGORY_NAME = 'Exchange Rates';
+export const DEFAULT_LOCAL_CURRENCY = Currency.Ils;
