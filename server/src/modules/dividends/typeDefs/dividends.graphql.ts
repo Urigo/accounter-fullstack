071770f05fc4b901fa810dff5a4dcba5a70cccc2
gilgardosh <gilgardosh@gmail.com>
2023-11-30 15:31:00 +0200

    prettier minor fix

--- server/src/modules/dividends/typeDefs/dividends.graphql.ts
+++ server/src/modules/dividends/typeDefs/dividends.graphql.ts
@@ -2,7 +2,6 @@ import { gql } from 'graphql-modules';
 
 // eslint-disable-next-line import/no-default-export
 export default gql`
-  " mock "
-  scalar # eslint-disable-next-line @graphql-eslint/no-unreachable-types
-  DividendMock
+  " mock " # eslint-disable-next-line @graphql-eslint/no-unreachable-types
+  scalar DividendMock
 `;
