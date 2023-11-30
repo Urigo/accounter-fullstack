820149b78cd09ba07426ebb529528803a9a022e8
gilgardosh <gilgardosh@gmail.com>
2023-11-30 10:20:46 +0200

    .

--- server/src/modules/dividends/typeDefs/dividends.graphql.ts
+++ server/src/modules/dividends/typeDefs/dividends.graphql.ts
@@ -3,6 +3,6 @@ import { gql } from 'graphql-modules';
 // eslint-disable-next-line import/no-default-export
 export default gql`
   " mock "
-  # eslint-disable-next-line @graphql-eslint/no-unreachable-types
-  scalar DividendMock
+  scalar # eslint-disable-next-line @graphql-eslint/no-unreachable-types
+  DividendMock
 `;
