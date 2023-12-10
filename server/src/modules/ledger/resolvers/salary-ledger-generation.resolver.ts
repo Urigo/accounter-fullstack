7e88f2fbc201cf3c96aa49bc2f48755e0f6364c9
gilgardosh <gilgardosh@gmail.com>
2023-12-10 10:36:36 +0200

    Enhance salary charge ledger generation

--- server/src/modules/ledger/resolvers/salary-ledger-generation.resolver.ts
+++ server/src/modules/ledger/resolvers/salary-ledger-generation.resolver.ts
@@ -5,17 +5,27 @@ import { FinancialAccountsProvider } from '@modules/financial-accounts/providers
 import { TaxCategoriesProvider } from '@modules/financial-entities/providers/tax-categories.provider.js';
 import { SalariesProvider } from '@modules/salaries/providers/salaries.provider.js';
 import { TransactionsProvider } from '@modules/transactions/providers/transactions.provider.js';
-import type { currency, IGetTransactionsByChargeIdsResult } from '@modules/transactions/types.js';
+import type { currency } from '@modules/transactions/types.js';
 import {
+  COMPENSATION_FUND_EXPENSES_TAX_CATEGORY_ID,
   DEFAULT_LOCAL_CURRENCY,
   EXCHANGE_RATE_CATEGORY_NAME,
+  PENSION_EXPENSES_TAX_CATEGORY_ID,
   SALARY_BATCHED_BUSINESSES,
+  SALARY_EXPENSES_TAX_CATEGORY_ID,
+  SOCIAL_SECURITY_EXPENSES_TAX_CATEGORY_ID,
+  TAX_DEDUCTIONS_BUSINESS_ID,
+  TRAINING_FUND_EXPENSES_TAX_CATEGORY_ID,
+  ZKUFOT_EXPENSES_TAX_CATEGORY_ID,
+  ZKUFOT_INCOME_TAX_CATEGORY_ID,
 } from '@shared/constants';
 import { Maybe, ResolverFn, ResolversParentTypes, ResolversTypes } from '@shared/gql-types';
-import type { LedgerProto } from '@shared/types';
+import type { CounterAccountProto, LedgerProto, StrictLedgerProto } from '@shared/types';
 import { generateEntriesFromSalaryRecords } from '../helpers/salary-charge-ledger.helper.js';
 import {
+  getLedgerBalanceInfo,
   getTaxCategoryNameByAccountCurrency,
+  updateLedgerBalanceByEntry,
   validateTransactionBasicVariables,
 } from '../helpers/utils.helper.js';
 
@@ -29,7 +39,7 @@ export const generateLedgerRecordsForSalary: ResolverFn<
 
   try {
     // validate ledger records are balanced
-    let ledgerBalance = 0;
+    const ledgerBalance = new Map<string, { amount: number; entity: CounterAccountProto }>();
 
     const dates = new Set<number>();
     const currencies = new Set<currency>();
@@ -39,34 +49,116 @@ export const generateLedgerRecordsForSalary: ResolverFn<
     // generate ledger from salary records
     const transactionDate =
       charge.transactions_min_debit_date ?? charge.transactions_min_event_date;
-    if (transactionDate) {
-      transactionDate.setDate(transactionDate.getDate() - 2); // adjusted date to match exchange rate of transaction initiation date
+    if (!transactionDate) {
+      throw new GraphQLError(`Charge ID="${charge.id}" is missing transaction date`);
+    }
 
-      const salaryRecords = await injector
-        .get(SalariesProvider)
-        .getSalaryRecordsByChargeIdLoader.load(charge.id);
+    // Get relevant data for generation
+    const salaryRecordsPromise = injector
+      .get(SalariesProvider)
+      .getSalaryRecordsByChargeIdLoader.load(charge.id);
+    const transactionsPromise = injector
+      .get(TransactionsProvider)
+      .getTransactionsByChargeIDLoader.load(chargeId);
+    const [salaryRecords, transactions] = await Promise.all([
+      salaryRecordsPromise,
+      transactionsPromise,
+    ]);
 
-      const { entries, balanceDelta } = generateEntriesFromSalaryRecords(
-        salaryRecords,
-        charge,
-        transactionDate,
-      );
+    // generate ledger from salary records
+    const {
+      entries,
+      salaryExpensesAmount,
+      socialSecurityExpensesAmount,
+      pensionFundExpensesAmount,
+      compensationProvidentFundExpensesAmount,
+      trainingFundExpensesAmount,
+      zkufotAmount,
+      month,
+    } = generateEntriesFromSalaryRecords(salaryRecords, charge, transactionDate);
 
-      accountingLedgerEntries.push(...entries);
-      ledgerBalance += balanceDelta;
-      dates.add(transactionDate.getTime());
-      currencies.add(DEFAULT_LOCAL_CURRENCY);
+    for (const ledgerEntry of entries) {
+      accountingLedgerEntries.push(ledgerEntry);
+      updateLedgerBalanceByEntry(ledgerEntry, ledgerBalance);
+      dates.add(ledgerEntry.valueDate.getTime());
+    }
+    currencies.add(DEFAULT_LOCAL_CURRENCY);
+
+    // generate monthly ledger entries
+    const monthlyEntriesSet: { taxCategoryId: string; amount: number; isCredit: boolean }[] = [
+      {
+        taxCategoryId: ZKUFOT_EXPENSES_TAX_CATEGORY_ID,
+        amount: zkufotAmount,
+        isCredit: false,
+      },
+      {
+        taxCategoryId: ZKUFOT_INCOME_TAX_CATEGORY_ID,
+        amount: zkufotAmount,
+        isCredit: true,
+      },
+      {
+        taxCategoryId: SOCIAL_SECURITY_EXPENSES_TAX_CATEGORY_ID,
+        amount: socialSecurityExpensesAmount,
+        isCredit: false,
+      },
+      {
+        taxCategoryId: SALARY_EXPENSES_TAX_CATEGORY_ID,
+        amount: salaryExpensesAmount,
+        isCredit: false,
+      },
+      {
+        taxCategoryId: TRAINING_FUND_EXPENSES_TAX_CATEGORY_ID,
+        amount: trainingFundExpensesAmount,
+        isCredit: false,
+      },
+      {
+        taxCategoryId: PENSION_EXPENSES_TAX_CATEGORY_ID,
+        amount: pensionFundExpensesAmount,
+        isCredit: false,
+      },
+      {
+        taxCategoryId: COMPENSATION_FUND_EXPENSES_TAX_CATEGORY_ID,
+        amount: compensationProvidentFundExpensesAmount,
+        isCredit: false,
+      },
+    ];
+    const montylyEntries = await Promise.all(
+      monthlyEntriesSet
+        .filter(({ amount }) => amount !== 0)
+        .map(async ({ taxCategoryId, amount, isCredit }) => {
+          const taxCategory = await injector
+            .get(TaxCategoriesProvider)
+            .taxCategoryByIDsLoader.load(taxCategoryId);
+          if (!taxCategory) {
+            throw new GraphQLError(`Tax category "${taxCategoryId}" not found`);
+          }
+
+          const ledgerEntry: LedgerProto = {
+            id: taxCategory.id,
+            invoiceDate: transactionDate,
+            valueDate: transactionDate,
+            currency: DEFAULT_LOCAL_CURRENCY,
+            ...(isCredit ? { creditAccountID1: taxCategory } : { debitAccountID1: taxCategory }),
+            localCurrencyCreditAmount1: amount,
+            localCurrencyDebitAmount1: amount,
+            description: `${month} salary: ${taxCategory.name}`,
+            isCreditorCounterparty: false, // TODO: check
+            ownerId: charge.owner_id,
+          };
+
+          return ledgerEntry;
+        }),
+    );
+
+    for (const ledgerEntry of montylyEntries) {
+      accountingLedgerEntries.push(ledgerEntry);
+      updateLedgerBalanceByEntry(ledgerEntry, ledgerBalance);
+      dates.add(ledgerEntry.valueDate.getTime());
     }
 
     // generate ledger from transactions
-    let transactions: Array<IGetTransactionsByChargeIdsResult> = [];
     const financialAccountLedgerEntries: LedgerProto[] = [];
     let hasBatchedCounterparty: boolean = false;
-
-    // Get all transactions
-    transactions = await injector
-      .get(TransactionsProvider)
-      .getTransactionsByChargeIDLoader.load(chargeId);
 
     // for each transaction, create a ledger record
     for (const transaction of transactions) {
@@ -106,15 +198,15 @@ export const generateLedgerRecordsForSalary: ResolverFn<
 
       const isCreditorCounterparty = amount > 0;
 
-      financialAccountLedgerEntries.push({
+      const ledgerEntry: StrictLedgerProto = {
         id: transaction.id,
         invoiceDate: transaction.event_date,
         valueDate,
         currency,
-        creditAccountID1: isCreditorCounterparty ? undefined : taxCategory,
+        creditAccountID1: isCreditorCounterparty ? transactionBusinessId : taxCategory,
         creditAmount1: foreignAmount ? Math.abs(foreignAmount) : undefined,
         localCurrencyCreditAmount1: Math.abs(amount),
-        debitAccountID1: isCreditorCounterparty ? taxCategory : undefined,
+        debitAccountID1: isCreditorCounterparty ? taxCategory : transactionBusinessId,
         debitAmount1: foreignAmount ? Math.abs(foreignAmount) : undefined,
         localCurrencyDebitAmount1: Math.abs(amount),
         description: transaction.source_description ?? undefined,
@@ -122,15 +214,18 @@ export const generateLedgerRecordsForSalary: ResolverFn<
         isCreditorCounterparty,
         ownerId: charge.owner_id,
         currencyRate: transaction.currency_rate ? Number(transaction.currency_rate) : undefined,
-      });
+      };
 
-      ledgerBalance += amount;
+      financialAccountLedgerEntries.push(ledgerEntry);
+      updateLedgerBalanceByEntry(ledgerEntry, ledgerBalance);
       dates.add(valueDate.getTime());
       currencies.add(currency);
     }
 
+    const { balanceSum } = getLedgerBalanceInfo(ledgerBalance);
+
     const miscLedgerEntries: LedgerProto[] = [];
-    if (Math.abs(ledgerBalance) > 0.005) {
+    if (Math.abs(balanceSum) > 0.005) {
       const hasMultipleDates = dates.size > 1;
       const hasForeignCurrency = currencies.size > (currencies.has(DEFAULT_LOCAL_CURRENCY) ? 1 : 0);
       if (hasMultipleDates && hasForeignCurrency) {
@@ -144,11 +239,11 @@ export const generateLedgerRecordsForSalary: ResolverFn<
           throw new GraphQLError(`Tax category "${EXCHANGE_RATE_CATEGORY_NAME}" not found`);
         }
 
-        const amount = Math.abs(ledgerBalance);
+        const amount = Math.abs(balanceSum);
 
-        const isCreditorCounterparty = ledgerBalance < 0;
+        const isCreditorCounterparty = balanceSum < 0;
 
-        miscLedgerEntries.push({
+        const ledgerEntry = {
           id: transactionEntry.id,
           creditAccountID1: isCreditorCounterparty ? undefined : exchangeCategory,
           creditAmount1: undefined,
@@ -162,7 +257,10 @@ export const generateLedgerRecordsForSalary: ResolverFn<
           valueDate: transactionEntry.valueDate,
           currency: transactionEntry.currency, // NOTE: this field is dummy
           ownerId: transactionEntry.ownerId,
-        });
+        };
+
+        miscLedgerEntries.push(ledgerEntry);
+        updateLedgerBalanceByEntry(ledgerEntry, ledgerBalance);
       } else {
         throw new GraphQLError(
           `Failed to balance: ${
@@ -172,6 +270,9 @@ export const generateLedgerRecordsForSalary: ResolverFn<
       }
     }
 
+    const allowedUnbalancedBusinesses = new Set([TAX_DEDUCTIONS_BUSINESS_ID]);
+    const ledgerBalanceInfo = getLedgerBalanceInfo(ledgerBalance, allowedUnbalancedBusinesses);
+
     // handle salary batched charges
     if (hasBatchedCounterparty) {
       // TODO: implement
@@ -180,6 +281,7 @@ export const generateLedgerRecordsForSalary: ResolverFn<
 
     return {
       records: [...accountingLedgerEntries, ...financialAccountLedgerEntries, ...miscLedgerEntries],
+      balance: ledgerBalanceInfo,
     };
   } catch (e) {
     return {
