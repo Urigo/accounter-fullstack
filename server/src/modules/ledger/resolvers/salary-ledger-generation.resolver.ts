b36d68e6277639615788fd3b883aee000fa60c9d
gilgardosh <gilgardosh@gmail.com>
2024-01-01 21:53:01 +0200

    Better handle fee transactions ledger generation

--- server/src/modules/ledger/resolvers/salary-ledger-generation.resolver.ts
+++ server/src/modules/ledger/resolvers/salary-ledger-generation.resolver.ts
@@ -1,5 +1,4 @@
 import { GraphQLError } from 'graphql';
-import { Injector } from 'graphql-modules';
 import { ExchangeProvider } from '@modules/exchange-rates/providers/exchange.provider.js';
 import { FinancialAccountsProvider } from '@modules/financial-accounts/providers/financial-accounts.provider.js';
 import { TaxCategoriesProvider } from '@modules/financial-entities/providers/tax-categories.provider.js';
@@ -12,12 +11,11 @@ import {
   BATCHED_PENSION_BUSINESS_ID,
   DEFAULT_LOCAL_CURRENCY,
   EXCHANGE_RATE_CATEGORY_NAME,
-  FEE_CATEGORY_NAME,
   SALARY_BATCHED_BUSINESSES,
 } from '@shared/constants';
 import { Maybe, ResolverFn, ResolversParentTypes, ResolversTypes } from '@shared/gql-types';
 import type { CounterAccountProto, LedgerProto, StrictLedgerProto } from '@shared/types';
-import { isSupplementalFeeTransaction, splitFeeTransactions } from '../helpers/fee-transactions.js';
+import { getEntriesFromFeeTransaction, splitFeeTransactions } from '../helpers/fee-transactions.js';
 import { generateEntriesFromSalaryRecords } from '../helpers/salary-charge-ledger.helper.js';
 import {
   generatePartialLedgerEntry,
@@ -25,7 +23,6 @@ import {
   getTaxCategoryNameByAccountCurrency,
   updateLedgerBalanceByEntry,
   ValidateTransaction,
-  validateTransactionBasicVariables,
   validateTransactionRequiredVariables,
 } from '../helpers/utils.helper.js';
 import { SalariesLedgerProvider } from '../providers/salaries-ledger.provider.js';
@@ -35,14 +32,15 @@ import { generateLedgerRecordsForCommonCharge } from './common-ledger-generation
 export const generateLedgerRecordsForSalary: ResolverFn<
   Maybe<ResolversTypes['GeneratedLedgerRecords']>,
   ResolversParentTypes['Charge'],
-  { injector: Injector },
+  GraphQLModules.Context,
   object
-> = async (charge, _args, { injector }, _info) => {
+> = async (charge, _args, context, _info) => {
   if (charge.business_array?.length === 1) {
     // for one business, use the default ledger generation
-    return generateLedgerRecordsForCommonCharge(charge, _args, { injector }, _info);
+    return generateLedgerRecordsForCommonCharge(charge, _args, context, _info);
   }
   const chargeId = charge.id;
+  const { injector } = context;
 
   try {
     // validate ledger records are balanced
@@ -296,84 +294,12 @@ export const generateLedgerRecordsForSalary: ResolverFn<
     // create a ledger record for fee transactions
     const feeFinancialAccountLedgerEntries: LedgerProto[] = [];
     const feeFinancialAccountLedgerEntriesPromises = feeTransactions.map(async transaction => {
-      if (!transaction.is_fee) {
-        return;
-      }
-
-      const isSupplementalFee = isSupplementalFeeTransaction(transaction);
-      const { currency, valueDate, transactionBusinessId } =
-        validateTransactionBasicVariables(transaction);
-
-      let amount = Number(transaction.amount);
-      let foreignAmount: number | undefined = undefined;
-
-      if (currency !== DEFAULT_LOCAL_CURRENCY) {
-        // get exchange rate for currency
-        const exchangeRate = await injector
-          .get(ExchangeProvider)
-          .getExchangeRates(currency, DEFAULT_LOCAL_CURRENCY, valueDate);
-
-        foreignAmount = amount;
-        // calculate amounts in ILS
-        amount = exchangeRate * amount;
-      }
-
-      const feeTaxCategory = await injector
-        .get(TaxCategoriesProvider)
-        .taxCategoryByNamesLoader.load(FEE_CATEGORY_NAME);
-      if (!feeTaxCategory) {
-        throw new GraphQLError(`Tax category "${FEE_CATEGORY_NAME}" not found`);
-      }
-
-      const isCreditorCounterparty = amount > 0;
-
-      let mainAccount: CounterAccountProto = transactionBusinessId;
-
-      const partialLedgerEntry: LedgerProto = {
-        id: transaction.id,
-        invoiceDate: transaction.event_date,
-        valueDate,
-        currency,
-        creditAmount1: foreignAmount ? Math.abs(foreignAmount) : undefined,
-        localCurrencyCreditAmount1: Math.abs(amount),
-        debitAmount1: foreignAmount ? Math.abs(foreignAmount) : undefined,
-        localCurrencyDebitAmount1: Math.abs(amount),
-        description: transaction.source_description ?? undefined,
-        reference1: transaction.source_id,
-        isCreditorCounterparty: isSupplementalFee
-          ? isCreditorCounterparty
-          : !isCreditorCounterparty,
-        ownerId: charge.owner_id,
-        currencyRate: transaction.currency_rate ? Number(transaction.currency_rate) : undefined,
-        chargeId,
-      };
-
-      if (isSupplementalFee) {
-        const account = await injector
-          .get(FinancialAccountsProvider)
-          .getFinancialAccountByAccountIDLoader.load(transaction.account_id);
-        if (!account) {
-          throw new GraphQLError(`Transaction ID="${transaction.id}" is missing account`);
-        }
-        const taxCategoryName = getTaxCategoryNameByAccountCurrency(account, currency);
-        const businessTaxCategory = await injector
-          .get(TaxCategoriesProvider)
-          .taxCategoryByNamesLoader.load(taxCategoryName);
-        if (!businessTaxCategory) {
-          throw new GraphQLError(`Account ID="${account.id}" is missing tax category`);
-        }
-
-        mainAccount = businessTaxCategory;
-      }
-
-      const ledgerEntry: StrictLedgerProto = {
-        ...partialLedgerEntry,
-        creditAccountID1: isCreditorCounterparty ? feeTaxCategory : mainAccount,
-        debitAccountID1: isCreditorCounterparty ? mainAccount : feeTaxCategory,
-      };
-
-      feeFinancialAccountLedgerEntries.push(ledgerEntry);
-      updateLedgerBalanceByEntry(ledgerEntry, ledgerBalance);
+      await getEntriesFromFeeTransaction(transaction, charge, context).then(ledgerEntries => {
+        feeFinancialAccountLedgerEntries.push(...ledgerEntries);
+        ledgerEntries.map(ledgerEntry => {
+          updateLedgerBalanceByEntry(ledgerEntry, ledgerBalance);
+        });
+      });
     });
     entriesPromises.push(...feeFinancialAccountLedgerEntriesPromises);
 
