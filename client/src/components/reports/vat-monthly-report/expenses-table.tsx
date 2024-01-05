e097aeedc1a068f0504cb1d2e5036e0947834a2d
gilgardosh <gilgardosh@gmail.com>
2024-01-05 14:46:29 +0200

    fix

--- client/src/components/reports/vat-monthly-report/expenses-table.tsx
+++ client/src/components/reports/vat-monthly-report/expenses-table.tsx
@@ -4,6 +4,7 @@ import { ActionIcon, Table } from '@mantine/core';
 import { VatReportExpensesFieldsFragmentDoc } from '../../../gql/graphql.js';
 import { FragmentType, getFragmentData } from '../../../gql/index.js';
 import { formatStringifyAmount } from '../../../helpers';
+import { AccountantApproval } from './cells/accountant-approval.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-expressions -- used by codegen
 /* GraphQL */ `
