e0fbff47c5f25c01a6a10b2418142eae494a23d4
Gil Gardosh <gilgardosh@gmail.com>
2024-01-18 18:45:21 +0200

    Add LedgerProvider

--- server/src/modules/ledger/index.ts
+++ server/src/modules/ledger/index.ts
@@ -3,6 +3,7 @@ import { createModule } from 'graphql-modules';
 import { BalanceCancellationProvider } from './providers/balance-cancellation.provider.js';
 import { UnbalancedBusinessesProvider } from './providers/unbalanced-businesses.provider.js';
 import { ledgerResolvers } from './resolvers/ledger.resolver.js';
+import { LedgerProvider } from './providers/ledger.provider.js';
 
 const __dirname = new URL('.', import.meta.url).pathname;
 
@@ -11,7 +12,7 @@ export const ledgerModule = createModule({
   dirname: __dirname,
   typeDefs: [ledger],
   resolvers: [ledgerResolvers],
-  providers: () => [UnbalancedBusinessesProvider, BalanceCancellationProvider],
+  providers: () => [LedgerProvider, UnbalancedBusinessesProvider, BalanceCancellationProvider],
 });
 
 export * as LedgerTypes from './types.js';
--- server/src/modules/ledger/types.ts
+++ server/src/modules/ledger/types.ts
@@ -1,4 +1,5 @@
 export * from './__generated__/types.js';
+export * from './__generated__/ledger.types.js';
 export * from './__generated__/unbalanced-businesses.types.js';
 export {
   IGetBalanceCancellationByChargesIdsParams,
