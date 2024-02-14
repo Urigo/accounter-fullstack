fafc2adb1b191f0ba55751b1e84de9cf111295d3
Gil Gardosh <gilgardosh@gmail.com>
2024-02-14 09:56:38 +0200

    cleanups

--- packages/server/src/modules/ledger/types.ts
+++ packages/server/src/modules/ledger/types.ts
@@ -1,8 +1,8 @@
 export * from './__generated__/types.js';
 export * from './__generated__/ledger.types.js';
 export * from './__generated__/unbalanced-businesses.types.js';
-export {
-  type IGetBalanceCancellationByChargesIdsParams,
-  type IGetBalanceCancellationByChargesIdsResult,
-  type IGetBalanceCancellationByChargesIdsQuery,
+export type {
+  IGetBalanceCancellationByChargesIdsParams,
+  IGetBalanceCancellationByChargesIdsResult,
+  IGetBalanceCancellationByChargesIdsQuery,
 } from './__generated__/balance-cancellation.types.js';
