17fa2e889e9dd0d0d05a604a9f46793d122e0778
Gil Gardosh <gilgardosh@gmail.com>
2024-02-19 17:03:57 +0200

    Enhancement: PgTyped config adjusted to local+staging envs (#437)
    * Enhancement: PgTyped config adjusted to local+staging envs
    
    * prettier

---
+++ packages/server/scripts/generate-local-pgconfig.cjs
@@ -0,0 +1,22 @@
+const { readFileSync, writeFileSync } = require('node:fs');
+
+var pgconfigPath = 'pgconfig.json';
+
+if (process.env['POSTGRES_SSL'] !== '1') {
+    try {
+        const data = readFileSync('./pgconfig.json');
+        const config = JSON.parse(data);
+    
+        if (config?.db?.ssl ) {
+            delete config.db.ssl;
+        }
+    
+        writeFileSync('./pgconfig.local.json', JSON.stringify(config, null, 2), 'utf8');
+
+        pgconfigPath = 'pgconfig.local.json';
+    } catch (err) {
+        /* empty */
+    }
+}
+
+console.log(pgconfigPath);
