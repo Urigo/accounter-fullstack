7db2589c3ca65266f02f4ad7edb3807edd08c909
Gil Gardosh <gilgardosh@gmail.com>
2024-02-26 09:44:27 +0200

    Cross year charges ledger (#449)

--- packages/migrations/src/run-pg-migrations.ts
+++ packages/migrations/src/run-pg-migrations.ts
@@ -1,10 +1,14 @@
 import { type DatabasePool } from 'slonik';
 import migration_2024_01_29T13_15_23_initial from './actions/2024-01-29T13-15-23.initial.js';
+import migration_2024_02_22T21_37_11_charge_year_of_relevance from './actions/2024-02-22T21-37-11.charge-year-of-relevance.js';
 import { runMigrations } from './pg-migrator.js';
 
 export const runPGMigrations = (args: { slonik: DatabasePool; runTo?: string }) =>
   runMigrations({
     slonik: args.slonik,
     runTo: args.runTo,
-    migrations: [migration_2024_01_29T13_15_23_initial],
+    migrations: [
+      migration_2024_01_29T13_15_23_initial,
+      migration_2024_02_22T21_37_11_charge_year_of_relevance,
+    ],
   });
