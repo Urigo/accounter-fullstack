afe02ad3180e81c9e0d1df435b0e66ecddb9e467
Gil Gardosh <gilgardosh@gmail.com>
2024-03-17 23:52:10 +0200

    add deposit transaction auto-merge to migration

--- packages/migrations/src/actions/2024-03-17T23-10-10.filter-distinct-transactions-currencies-on-charge.ts
+++ packages/migrations/src/actions/2024-03-17T23-10-10.filter-distinct-transactions-currencies-on-charge.ts
@@ -173,5 +173,131 @@ export default {
                                     unnest(ARRAY [ledger_records.credit_entity1, ledger_records.credit_entity2, ledger_records.debit_entity1, ledger_records.debit_entity2]) AS financial_entity
                               FROM accounter_schema.ledger_records) l2
                         GROUP BY l2.charge_id) l ON l.charge_id = c.id;
+
+        create or replace function accounter_schema.insert_poalim_usd_transaction_handler() returns trigger
+            language plpgsql
+        as
+        $$
+            DECLARE
+                merged_id UUID;
+                account_id_var UUID;
+                owner_id_var UUID;
+                charge_id_var UUID = NULL;
+                is_conversion BOOLEAN = false;
+                is_fee BOOLEAN = false;
+            BEGIN
+                -- Create merged raw transactions record:
+                INSERT INTO accounter_schema.transactions_raw_list (poalim_usd_id)
+                VALUES (NEW.id)
+                RETURNING id INTO merged_id;
+
+                -- get account and owner IDs
+                SELECT INTO account_id_var, owner_id_var
+                    id, owner
+                FROM accounter_schema.financial_accounts
+                WHERE account_number = NEW.account_number::TEXT;
+
+                -- handle conversions
+                IF (new.activity_type_code IN (884, 957,1058)) THEN
+                    is_conversion = true;
+
+                    -- check if matching charge exists:
+                    SELECT t.charge_id
+                    INTO charge_id_var
+                    FROM (
+                        SELECT 'eur', id, reference_number, reference_catenated_number, value_date, event_amount FROM accounter_schema.poalim_eur_account_transactions
+                        WHERE activity_type_code IN (884, 957,1058)
+                        UNION
+                        SELECT 'gbp', id, reference_number, reference_catenated_number, value_date, event_amount FROM accounter_schema.poalim_gbp_account_transactions
+                        WHERE activity_type_code IN (884, 957,1058)
+                        UNION
+                        SELECT 'ils', id, reference_number, reference_catenated_number, value_date, event_amount FROM accounter_schema.poalim_ils_account_transactions
+                        WHERE text_code IN (22, 23)) AS s
+                    LEFT JOIN accounter_schema.transactions_raw_list tr
+                    ON COALESCE(tr.poalim_ils_id, tr.poalim_eur_id, tr.poalim_gbp_id, tr.poalim_usd_id) = s.id
+                    LEFT JOIN accounter_schema.transactions t
+                    ON tr.id = t.source_id
+                    WHERE t.charge_id IS NOT NULL
+                    AND s.reference_number = NEW.reference_number
+                    AND s.reference_catenated_number = NEW.reference_catenated_number
+                    AND s.value_date = NEW.value_date;
+
+                    -- update charge's tag to 'conversion'
+                    IF (charge_id_var IS NOT NULL) THEN
+                        INSERT INTO accounter_schema.tags (charge_id, tag_name)
+                        VALUES (charge_id_var, 'conversion')
+                        ON CONFLICT DO NOTHING;
+                    END IF;
+                END IF;
+
+                -- handle bank deposits
+                IF (new.activity_type_code IN (1376, 1384, 169, 171, 172)) THEN
+                    -- check if matching charge exists:
+                    SELECT t.charge_id
+                    INTO charge_id_var
+                    FROM (
+                        SELECT 'eur', id, reference_number, reference_catenated_number, value_date, event_amount FROM accounter_schema.poalim_eur_account_transactions
+                        WHERE activity_type_code IN (1376, 1384)
+                        UNION
+                        SELECT 'gbp', id, reference_number, reference_catenated_number, value_date, event_amount FROM accounter_schema.poalim_gbp_account_transactions
+                        WHERE activity_type_code IN (1376, 1384)
+                        UNION
+                        SELECT 'ils', id, reference_number, reference_catenated_number, value_date, event_amount FROM accounter_schema.poalim_ils_account_transactions
+                        WHERE text_code IN (113, 117, 457)) AS s
+                    LEFT JOIN accounter_schema.transactions_raw_list tr
+                    ON COALESCE(tr.poalim_ils_id, tr.poalim_eur_id, tr.poalim_gbp_id, tr.poalim_usd_id) = s.id
+                    LEFT JOIN accounter_schema.transactions t
+                    ON tr.id = t.source_id
+                    WHERE t.charge_id IS NOT NULL
+                    AND s.reference_number = NEW.reference_number
+                    AND s.reference_catenated_number = NEW.reference_catenated_number
+                    AND s.value_date = NEW.value_date;
+                END IF;
+
+                -- if no match, create new charge
+                IF (charge_id_var IS NULL) THEN
+                    INSERT INTO accounter_schema.charges (owner_id, is_conversion)
+                    VALUES (
+                        owner_id_var,
+                        is_conversion
+                    )
+                    RETURNING id INTO charge_id_var;
+                END IF;
+
+                -- check if new record is fee
+                IF (new.activity_type_code = 1279 AND new.event_amount BETWEEN 0 AND 30)
+                THEN is_fee = true;
+                END IF;
+
+                -- check if new record contains fees
+                -- TBD
+
+                -- create new transaction
+                INSERT INTO accounter_schema.transactions (account_id, charge_id, source_id, source_description, currency, event_date, debit_date, amount, current_balance, is_fee)
+                VALUES (
+                    account_id_var,
+                    charge_id_var,
+                    merged_id,
+                    concat(
+                        new.activity_description,
+                        ' ',
+                        coalesce(new.event_details, ''),
+                        ' ',
+                        coalesce(new.account_name, '')
+                    ),
+                    'USD',
+                    new.executing_date::text::date,
+                    new.value_date::text::date,
+                    (CASE
+                        WHEN new.event_activity_type_code = 2 THEN (new.event_amount * -1)
+                        ELSE new.event_amount END
+                    ),
+                    new.current_balance,
+                    is_fee
+                );
+
+                RETURN NEW;
+            END;
+            $$;
 `,
 } satisfies MigrationExecutor;
