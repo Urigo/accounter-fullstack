7425f384692422bbf96fa2f2c03c38ead2bf542b
Gil Gardosh <gilgardosh@gmail.com>
2024-03-18 20:26:49 +0200

    File upload fix

--- packages/server/src/modules/documents/helpers/green-invoice.helper.ts
+++ packages/server/src/modules/documents/helpers/green-invoice.helper.ts
@@ -8,30 +8,38 @@ export function normalizeDocumentType(
   rawType?:
     | mutation_addExpense_oneOf_0_allOf_0_documentType
     | query_searchDocuments_items_items_payment_items_ref_items
+    | number
     | null,
 ): DocumentType {
   if (!rawType) {
     return DocumentType.Unprocessed;
   }
   switch (rawType) {
+    case 20:
     case '_20':
       // חשבון / אישור תשלום
       return DocumentType.Invoice;
+    case 300:
     case '_300':
       // חשבונית עסקה
       return DocumentType.Proforma;
+    case 305:
     case '_305':
       // חשבונית מס
       return DocumentType.Invoice;
+    case 320:
     case '_320':
       // חשבונית מס\קבלה
       return DocumentType.InvoiceReceipt;
+    case 330:
     case '_330':
       // חשבונית זיכוי
       return DocumentType.CreditInvoice;
+    case 400:
     case '_400':
       // קבלה
       return DocumentType.Receipt;
+    case 405:
     case '_405':
       // קבלה על תרומה
       return DocumentType.Unprocessed;
