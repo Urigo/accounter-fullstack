c49747df16054e8b440fd2112f3a032088279fa1
Gil Gardosh <gilgardosh@gmail.com>
2024-03-19 16:31:58 +0200

    enhance logic

--- packages/server/src/modules/ledger/helpers/cross-year-ledger.helper.ts
+++ packages/server/src/modules/ledger/helpers/cross-year-ledger.helper.ts
@@ -3,6 +3,9 @@ import { EXPENSES_TO_PAY_TAX_CATEGORY, INCOME_TO_COLLECT_TAX_CATEGORY } from '@s
 import type { LedgerProto } from '@shared/types';
 
 function divideAmount(yearsCount: number, amount?: number): number | undefined {
+  if (yearsCount === 1) {
+    return amount;
+  }
   return amount ? amount / yearsCount : undefined;
 }
 
@@ -38,28 +41,31 @@ export function handleCrossYearLedgerEntries(
   const crossYearEntries: LedgerProto[] = [];
 
   for (const entry of accountingLedgerEntries) {
-    if (years_of_relevance.length === 1) {
-      const yearOfRelevance = years_of_relevance[0];
+    const yearsCount = years_of_relevance.length;
+    const amounts = divideEntryAmounts(entry, yearsCount);
+
+    for (const yearDate of years_of_relevance) {
       if (
-        entry.invoiceDate.getFullYear() === yearOfRelevance.getFullYear() &&
-        entry.valueDate.getFullYear() === yearOfRelevance.getFullYear()
+        entry.invoiceDate.getFullYear() === yearDate.getFullYear() &&
+        entry.valueDate.getFullYear() === yearDate.getFullYear()
       ) {
-        crossYearEntries.push(entry);
+        crossYearEntries.push({ ...entry, ...amounts });
         continue;
       }
 
       const mediateTaxCategory = entry.isCreditorCounterparty
         ? EXPENSES_TO_PAY_TAX_CATEGORY
         : INCOME_TO_COLLECT_TAX_CATEGORY;
-      const isYearOfRelevancePrior =
-        entry.invoiceDate.getFullYear() > yearOfRelevance.getFullYear();
+
+      const isYearOfRelevancePrior = entry.invoiceDate.getFullYear() > yearDate.getFullYear();
       const yearOfRelevanceDates = isYearOfRelevancePrior
-        ? { invoiceDate: new Date(yearOfRelevance.getFullYear(), 11, 31) }
-        : { invoiceDate: new Date(yearOfRelevance.getFullYear(), 0, 1) };
+        ? { invoiceDate: new Date(yearDate.getFullYear(), 11, 31) }
+        : { invoiceDate: new Date(yearDate.getFullYear(), 0, 1) };
       crossYearEntries.push(
         {
           // first chronological entry
           ...entry,
+          ...amounts,
           ...(entry.isCreditorCounterparty
             ? { creditAccountID1: mediateTaxCategory }
             : { debitAccountID1: mediateTaxCategory }),
@@ -68,34 +74,13 @@ export function handleCrossYearLedgerEntries(
         {
           // second chronological entry
           ...entry,
+          ...amounts,
           ...(entry.isCreditorCounterparty
             ? { debitAccountID1: mediateTaxCategory }
             : { creditAccountID1: mediateTaxCategory }),
           ...(isYearOfRelevancePrior ? {} : yearOfRelevanceDates),
         },
       );
-    } else {
-      const yearsCount = years_of_relevance.length;
-      const amounts = divideEntryAmounts(entry, yearsCount);
-
-      for (const yearDate of years_of_relevance) {
-        if (
-          entry.invoiceDate.getFullYear() === yearDate.getFullYear() &&
-          entry.valueDate.getFullYear() === yearDate.getFullYear()
-        ) {
-          crossYearEntries.push({ ...entry, ...amounts });
-          continue;
-        }
-        const isYearOfRelevancePrior = entry.invoiceDate.getFullYear() > yearDate.getFullYear();
-        const yearOfRelevanceDates = isYearOfRelevancePrior
-          ? { invoiceDate: new Date(yearDate.getFullYear(), 11, 31) }
-          : { invoiceDate: new Date(yearDate.getFullYear(), 0, 1) };
-        crossYearEntries.push({
-          ...entry,
-          ...amounts,
-          ...yearOfRelevanceDates,
-        });
-      }
     }
   }
   return crossYearEntries;
