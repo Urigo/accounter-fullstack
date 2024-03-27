89e1eb48d455daad665b6abeb9f4377953b9bbca
Gil Gardosh <gilgardosh@gmail.com>
2024-03-27 16:42:54 +0200

    Slonik major bump (#527)

--- packages/migrations/src/index.ts
+++ packages/migrations/src/index.ts
@@ -4,20 +4,24 @@ import { createConnectionString } from './connection-string.js';
 import { env } from './environment.js';
 import { runPGMigrations } from './run-pg-migrations.js';
 
-const slonik = await createPool(createConnectionString(env.postgres), {
-  // 10 minute timeout per statement
-  statementTimeout: 10 * 60 * 1000,
-});
+async function migrations() {
+  const slonik = await createPool(createConnectionString(env.postgres), {
+    // 10 minute timeout per statement
+    statementTimeout: 10 * 60 * 1000,
+  });
 
-// This is used by production build of this package.
-// We are building a "cli" out of the package, so we need a workaround to pass the command to run.
+  // This is used by production build of this package.
+  // We are building a "cli" out of the package, so we need a workaround to pass the command to run.
 
-console.log('Running the UP migrations');
+  console.log('Running the UP migrations');
 
-try {
-  await runPGMigrations({ slonik });
-  process.exit(0);
-} catch (error) {
-  console.error(error);
-  process.exit(1);
+  try {
+    await runPGMigrations({ slonik });
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
+
+migrations();
