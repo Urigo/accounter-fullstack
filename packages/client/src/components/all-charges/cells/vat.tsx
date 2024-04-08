6405838cc36757ac43134d5492d1f5f8e1f903e5
Gil Gardosh <gilgardosh@gmail.com>
2024-04-08 21:03:26 +0300

    Fix: remove "missing" VAT indication for credit card charges (#580)

--- packages/client/src/components/all-charges/cells/vat.tsx
+++ packages/client/src/components/all-charges/cells/vat.tsx
@@ -69,7 +69,7 @@ export const Vat = ({ data }: Props): ReactElement => {
     <td>
       <div style={{ color: vatIssueFlag ? 'red' : 'green', whiteSpace: 'nowrap' }}>
         <Indicator inline size={12} disabled={!isError} color="red" zIndex="auto">
-          {vat ? vat.formatted : vatIssueFlag ? 'Missing' : null}
+          {vat?.formatted}
         </Indicator>
       </div>
     </td>
