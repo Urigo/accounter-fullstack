552125e4654b21887155ef6ae27be75d7b7fddde
Gil Gardosh <gilgardosh@gmail.com>
2024-04-14 13:36:28 +0300

    extend DB migration

--- packages/migrations/src/actions/2024-04-10T15-04-24.business-trips-attendees-dates.ts
+++ packages/migrations/src/actions/2024-04-10T15-04-24.business-trips-attendees-dates.ts
@@ -31,5 +31,23 @@ export default {
     FROM accounter_schema.business_trips_attendees
     GROUP BY business_trips_attendees.business_trip_id) a ON a.id = bt.id
   ORDER BY bt.id, a.from_date;
+
+  create or replace view accounter_schema.extended_business_trip_transactions(id, business_trip_id, transaction_id, category, date, value_date, amount, currency, employee_business_id, payed_by_employee) as
+  SELECT DISTINCT ON (btt.id) btt.id,
+    btt.business_trip_id,
+    t.id,
+    btt.category,
+    COALESCE (btt.date, t.event_date) as date,
+    COALESCE (btt.value_date, t.debit_date) as value_date,
+    COALESCE (btt.amount, t.amount) as amount,
+    COALESCE(btt.currency, t.currency),
+    btt.employee_business_id,
+    btt.payed_by_employee
+  FROM accounter_schema.business_trips_transactions btt
+    LEFT JOIN accounter_schema.transactions t ON t.id = btt.transaction_id
+  ORDER BY btt.id;
+
+  alter table accounter_schema.business_trips_transactions
+    alter column category set not null;
 `,
 } satisfies MigrationExecutor;
