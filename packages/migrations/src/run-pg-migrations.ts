f1b71bac39f51174a187ee95e2f063e302fcc823
Gil Gardosh <gilgardosh@gmail.com>
2024-05-19 18:29:08 +0300

    DB updated_at triggers fix (#678)

--- packages/migrations/src/run-pg-migrations.ts
+++ packages/migrations/src/run-pg-migrations.ts
@@ -11,6 +11,7 @@ import migration_2024_04_01T17_36_20_extend_charges_business_array_with_ledger f
 import migration_2024_04_10T15_04_24_business_trips_attendees_dates from './actions/2024-04-10T15-04-24.business-trips-attendees-dates.js';
 import migration_2024_05_15T10_23_55_creditcard_trigger_better_filtering from './actions/2024-05-15T10-23-55.creditcard-trigger-better-filtering.js';
 import migration_2024_05_15T21_55_55_creditcard_trigger_better_filtering2 from './actions/2024-05-15T21-55-55.creditcard-trigger-better-filtering2.js';
+import migration_2024_05_19T13_36_34_fix_updated_at_triggers from './actions/2024-05-19T13-36-34.fix-updated-at-triggers.js';
 import { runMigrations } from './pg-migrator.js';
 
 export const runPGMigrations = (args: { slonik: DatabasePool }) =>
@@ -29,5 +30,6 @@ export const runPGMigrations = (args: { slonik: DatabasePool }) =>
       migration_2024_04_10T15_04_24_business_trips_attendees_dates,
       migration_2024_05_15T10_23_55_creditcard_trigger_better_filtering,
       migration_2024_05_15T21_55_55_creditcard_trigger_better_filtering2,
+      migration_2024_05_19T13_36_34_fix_updated_at_triggers,
     ],
   });
