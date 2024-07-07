d7662e313f8ed5baaa5550c0c327f3595ef2a8ad
Gil Gardosh <gilgardosh@gmail.com>
2024-07-07 19:00:42 +0300

    Refactor exchange rates scraper

--- packages/old-accounter/src/data/currency.ts
+++ packages/old-accounter/src/data/currency.ts
@@ -1,10 +1,14 @@
-import { add, differenceInDays, format, sub } from 'date-fns';
+import { add, format, sub } from 'date-fns';
+import { XMLParser } from 'fast-xml-parser';
 import { Pool } from 'pg';
 import XML from 'pixl-xml';
 import puppeteer from 'puppeteer';
 import type { Page } from 'puppeteer';
 
 // TODO: Compare to this library: https://github.com/TobiasNickel/tXml
+
+const currencies = ['USD', 'EUR', 'GBP'] as const;
+type Currency = (typeof currencies)[number];
 
 async function getCurrencyRatesForDate(currentDate: Date, page: Page) {
   const url = `https://edge.boi.gov.il/FusionEdgeServer/sdmx/v2/data/dataflow/BOI.STATISTICS/EXR/1.0/RER_USD_ILS,RER_EUR_ILS,RER_GBP_ILS?startperiod=${format(
@@ -70,37 +74,95 @@ async function getCurrencyRatesForDate(currentDate: Date, page: Page) {
 
 export async function getCurrencyRates(pool: Pool) {
   const existingRatesQuery = `
-      SELECT exchange_date FROM accounter_schema.exchange_rates 
-      ORDER BY exchange_date DESC
-      LIMIT 1
+      SELECT * FROM accounter_schema.exchange_rates 
+      ORDER BY exchange_date ASC
     `;
   const existingRates = await pool.query(existingRatesQuery);
 
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-
-  for (
-    let currentDate = add(new Date(existingRates.rows[0].exchange_date), {
-      days: 1,
+  const dbData = new Map<string, Partial<Record<Currency, number | null>>>();
+  for (const rate of existingRates.rows) {
+    dbData.set(format(rate.exchange_date, 'yyyy-MM-dd'), {
+      USD: rate.usd ? Number(rate.usd) : null,
+      EUR: rate.eur ? Number(rate.eur) : null,
+      GBP: rate.gbp ? Number(rate.gbp) : null,
     });
-    differenceInDays(Date.now(), currentDate) >= 2;
-    currentDate = add(currentDate, { days: 1 })
-  ) {
-    await (async () => {
-      const currencyRates = await getCurrencyRatesForDate(currentDate, page);
+  }
 
-      if (currencyRates) {
+  const res = await fetch(
+    'https://edge.boi.gov.il/FusionEdgeServer/sdmx/v2/data/dataflow/BOI.STATISTICS/EXR/1.0/RER_USD_ILS,RER_EUR_ILS,RER_GBP_ILS',
+  );
+
+  const XMLdata = await res.text();
+
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+  });
+  const parsedXml = parser.parse(XMLdata);
+
+  if (!parsedXml?.['message:StructureSpecificData']?.['message:DataSet']?.Series?.length) {
+    console.log('No currency rates data fetched');
+    return;
+  }
+
+  const govData = new Map<string, Partial<Record<Currency, number | null>>>();
+  const series = parsedXml['message:StructureSpecificData']['message:DataSet']['Series'];
+  for (const currencyData of series) {
+    if (currencyData.Obs?.length) {
+      const currency = currencyData['@_BASE_CURRENCY'] as Currency;
+      if (!currencies.includes(currency)) {
+        console.log(`Unsupported currency base ${currency}`);
+        continue;
+      }
+      for (const obs of currencyData.Obs) {
+        if (!govData.has(obs['@_TIME_PERIOD'])) {
+          govData.set(obs['@_TIME_PERIOD'], {
+            [currency]: Number(obs['@_OBS_VALUE']),
+          });
+        } else {
+          govData.set(obs['@_TIME_PERIOD'], {
+            ...govData.get(obs['@_TIME_PERIOD']),
+            [currency]: Number(obs['@_OBS_VALUE']),
+          });
+        }
+      }
+    }
+  }
+
+  const newRecords: string[] = [];
+  const newValues = [];
+  let newValuesCount = 1;
+
+  for (const [date, rates] of govData) {
+    const dbRates = dbData.get(date);
+
+    if (dbRates) {
+      let diffCount = 1;
+      const updatesParts: string[] = [];
+      const values = [];
+      for (const currency of currencies) {
+        if (rates[currency] !== dbRates[currency]) {
+          if (dbRates[currency] === null) {
+            console.log(
+              `Difference in ${currency} rate for ${date}: currently empty, updating value to ${rates[currency]}`,
+            );
+            updatesParts.push(`${currency.toLowerCase()} = $${diffCount}`);
+            values.push(rates[currency] ?? null);
+            diffCount++;
+          } else {
+            console.log(
+              `Value of ${currency} rate for ${date} has changed! formerly ${dbRates[currency]}, now recorded as ${rates[currency]}. please address this manually.`,
+            );
+          }
+        }
+      }
+
+      if (updatesParts.length) {
+        values.push(date);
         const text = `
-            INSERT INTO accounter_schema.exchange_rates 
-            (exchange_date, usd, eur, gbp) VALUES ($1, $2, $3, $4) RETURNING *
-          `;
-
-        const values: any[] = [
-          format(currentDate, 'yyyy-MM-dd'),
-          currencyRates.dollarRate,
-          currencyRates.euroRate,
-          currencyRates.gbpRate,
-        ];
+          UPDATE accounter_schema.exchange_rates
+          SET ${updatesParts.join(', ')}
+          WHERE exchange_date = $${diffCount} RETURNING *
+        `;
 
         try {
           const res = await pool.query(text, values);
@@ -108,82 +170,33 @@ export async function getCurrencyRates(pool: Pool) {
           console.log(format(res.rows[0].exchange_date, 'yyyyMMdd'));
         } catch (error) {
           // TODO: Log important checks
-          console.log('error in insert - ', error);
-          // console.log('nothing');
+          console.log('error in update - ', error);
         }
       }
-    })();
+    } else if (date > '2009-12-31') {
+      newRecords.push(
+        `$${newValuesCount}, $${newValuesCount + 1}, $${newValuesCount + 2}, $${newValuesCount + 3}`,
+      );
+      newValues.push(date, rates.USD, rates.EUR, rates.GBP);
+      newValuesCount += 4;
+    }
   }
-}
 
-export async function compareCurrencyRatesToDB(pool: Pool) {
-  const existingRatesQuery = `
-      SELECT exchange_date FROM accounter_schema.exchange_rates 
-      ORDER BY exchange_date DESC
-      LIMIT 1
+  if (newRecords.length) {
+    const text = `
+      INSERT INTO accounter_schema.exchange_rates 
+      (exchange_date, usd, eur, gbp) VALUES (${newRecords.join('), (')}) RETURNING *
     `;
-  const existingRates = await pool.query(existingRatesQuery);
 
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-
-  for (
-    let currentDate = add(new Date(existingRates.rows[0].exchange_date), {
-      days: 1,
-    });
-    currentDate >= sub(new Date(), { years: 2 });
-    currentDate = sub(currentDate, { days: 1 })
-  ) {
-    await (async () => {
-      const currencyRates = await getCurrencyRatesForDate(currentDate, page);
-
-      if (currencyRates) {
-        const getDBForDate = `
-          SELECT * FROM accounter_schema.exchange_rates 
-          WHERE exchange_date = '${format(currentDate, 'yyyy-MM-dd')}';
-        `;
-        const existingRate = await pool.query(getDBForDate);
-
-        if (existingRate.rowCount === 0) {
-          console.log(`Not in DB but in website ${format(currentDate, 'yyyy-MM-dd')}`);
-
-          const text = `
-            INSERT INTO accounter_schema.exchange_rates 
-            (exchange_date, usd, eur) VALUES ($1, $2, $3) RETURNING *
-          `;
-
-          const values: any[] = [
-            format(currentDate, 'yyyy-MM-dd'),
-            currencyRates.dollarRate,
-            currencyRates.euroRate,
-          ];
-
-          try {
-            const res = await pool.query(text, values);
-            console.log(res.rows[0]);
-            console.log(format(res.rows[0].exchange_date, 'yyyyMMdd'));
-          } catch (error) {
-            // TODO: Log important checks
-            console.log('error in insert - ', error);
-            // console.log('nothing');
-          }
-        } else if (
-          parseFloat(currencyRates.dollarRate.toString()).toFixed(4) ==
-            parseFloat(existingRate.rows[0].usd).toFixed(4) &&
-          parseFloat(currencyRates.euroRate.toString()).toFixed(4) ==
-            parseFloat(existingRate.rows[0].eur).toFixed(4)
-        ) {
-          console.log(`Same for ${format(currentDate, 'yyyy-MM-dd')}`);
-        } else {
-          console.error(`Different for ${format(currentDate, 'yyyy-MM-dd')}`);
-          console.log(existingRate.rows[0].usd);
-          console.log(existingRate.rows[0].eur);
-          console.log(existingRate.rows[0].exchange_date);
-        }
-      } else {
-        console.log(`no currency rates for ${format(currentDate, 'yyyy-MM-dd')}`);
-      }
-    })();
+    try {
+      const res = await pool.query(text, newValues);
+      res.rows.map(row => {
+        console.log(row);
+        console.log(format(row.exchange_date, 'yyyyMMdd'));
+      });
+    } catch (error) {
+      // TODO: Log important checks
+      console.log('error in insert - ', error);
+    }
   }
-  await browser.close();
 }
