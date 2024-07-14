75e9498dee3fc2606e7ae5de73dae0f25b79d582
Gil Gardosh <gilgardosh@gmail.com>
2024-07-14 12:27:39 +0300

    lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -69,6 +69,7 @@ module.exports = {
                 'PCNRawData',
                 'SicknessDays',
                 'SortCode',
+                'Suggestions',
                 'TransactionSuggestions',
                 'UpdateChargeSuccessfulResult',
                 'VatReportResult',
