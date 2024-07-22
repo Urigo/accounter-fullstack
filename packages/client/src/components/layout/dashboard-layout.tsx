b7f937cb7c37a12b861ff7fc67ad17a72ecc7509
TuvalSimha <tuval.simha@gmail.com>
2024-07-22 18:12:14 +0300

    prettier

--- packages/client/src/components/layout/dashboard-layout.tsx
+++ packages/client/src/components/layout/dashboard-layout.tsx
@@ -1,8 +1,8 @@
 import { ReactElement } from 'react';
+import { useSidebar } from '../../hooks/use-sidebar';
 import { Footer } from './footer';
 import { Header } from './header';
 import { Sidebar } from './sidebar';
-import { useSidebar } from '../../hooks/use-sidebar';
 
 type DashboardLayoutProps = {
   filtersContext: ReactElement | null;
@@ -17,7 +17,9 @@ export function DashboardLayout({ children, filtersContext }: DashboardLayoutPro
       <Header />
       <div className="flex h-screen overflow-hidden bg-gray-100">
         <Sidebar />
-        <div className={`overflow-scroll flex flex-col justify-start gap-10 z-0 my-20 pl-10 flex-1 ${isMinimized ? 'pr-10' : 'pr-5'} transition-all`}>
+        <div
+          className={`overflow-scroll flex flex-col justify-start gap-10 z-0 my-20 pl-10 flex-1 ${isMinimized ? 'pr-10' : 'pr-5'} transition-all`}
+        >
           {children}
         </div>
       </div>
