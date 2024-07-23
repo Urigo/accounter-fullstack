5785fd5d44df61344cfb0a8b7af08271fd29010d
Tuval Simha <tuval.simha@Gmail.com>
2024-07-23 09:27:04 +0300

    Bug: missing scroll on all-charges screen (#956)
    * Bug: missing scroll on all-charges screen
    
    * ?
    
    * prettier

--- packages/client/src/components/layout/dashboard-layout.tsx
+++ packages/client/src/components/layout/dashboard-layout.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react';
-import { ScrollArea } from '../ui/scroll-area';
+import { useSidebar } from '../../hooks/use-sidebar';
 import { Footer } from './footer';
 import { Header } from './header';
 import { Sidebar } from './sidebar';
@@ -10,13 +10,17 @@ type DashboardLayoutProps = {
 };
 
 export function DashboardLayout({ children, filtersContext }: DashboardLayoutProps): ReactElement {
+  const { isMinimized } = useSidebar();
+
   return (
     <main>
       <Header />
       <div className="flex h-screen overflow-hidden bg-gray-100">
         <Sidebar />
-        <div className="z-0 flex-1 overflow-hidden pt-16 px-10 mb-20">
-          <ScrollArea className="h-full w-full">{children}</ScrollArea>
+        <div
+          className={`overflow-scroll flex flex-col justify-start gap-10 z-0 my-20 pl-10 flex-1 ${isMinimized ? 'pr-10' : 'pr-5'} transition-all`}
+        >
+          {children}
         </div>
       </div>
       <Footer filtersContext={filtersContext} />
