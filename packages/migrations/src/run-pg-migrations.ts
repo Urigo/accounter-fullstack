1be8ab89a5bb28536b856a069460a77d4509ffb3
Gil Gardosh <gilgardosh@gmail.com>
2024-07-25 21:20:34 +0300

    Enhance DB view tables (#973)

--- packages/migrations/src/run-pg-migrations.ts
+++ packages/migrations/src/run-pg-migrations.ts
@@ -29,6 +29,7 @@ import migration_2024_07_15T16_22_59_documents_table_reorder from './actions/202
 import migration_2024_07_18T13_43_02_enhance_charge_spreading from './actions/2024-07-18T13-43-02.enhance-charge-spreading.js';
 import migration_2024_07_23T12_58_43_edit_charge_type_enum1 from './actions/2024-07-23T12-58-43.edit-charge-type-enum1.js';
 import migration_2024_07_23T12_58_43_edit_charge_type_enum2 from './actions/2024-07-23T12-58-43.edit-charge-type-enum2.js';
+import migration_2024_07_25T14_10_36_enhance_view_tables from './actions/2024-07-25T14-10-36.enhance-view-tables.js';
 import { runMigrations } from './pg-migrator.js';
 
 export const runPGMigrations = (args: { slonik: DatabasePool }) =>
@@ -65,5 +66,6 @@ export const runPGMigrations = (args: { slonik: DatabasePool }) =>
       migration_2024_07_18T13_43_02_enhance_charge_spreading,
       migration_2024_07_23T12_58_43_edit_charge_type_enum1,
       migration_2024_07_23T12_58_43_edit_charge_type_enum2,
+      migration_2024_07_25T14_10_36_enhance_view_tables,
     ],
   });
