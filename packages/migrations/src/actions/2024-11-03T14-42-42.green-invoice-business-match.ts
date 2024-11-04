b3c726c0ac85cdda089fbbb7a5362941d99f1117
Gil Gardosh <gilgardosh@gmail.com>
2024-11-04 17:39:13 +0200

    .

--- packages/migrations/src/actions/2024-11-03T14-42-42.green-invoice-business-match.ts
+++ packages/migrations/src/actions/2024-11-03T14-42-42.green-invoice-business-match.ts
@@ -5,16 +5,17 @@ export default {
   run: ({ sql }) => sql`
     create table if not exists accounter_schema.businesses_green_invoice_match
     (
-        business_id      uuid not null
+        business_id      uuid                                                                              not null
             constraint businesses_green_invoice_match_pk
                 primary key
             constraint businesses_green_invoice_match_businesses_id_fk
                 references accounter_schema.businesses,
-        green_invoice_id uuid not null
+        green_invoice_id uuid                                                                              not null
             constraint businesses_green_invoice_match_pk_2
                 unique,
         remark           text,
-        emails           text[]
+        emails           text[],
+        document_type    accounter_schema.document_type default 'PROFORMA'::accounter_schema.document_type not null
     );
 
     create unique index if not exists businesses_green_invoice_match_business_id_uindex
