50e8c0a16d6ba0b05be26417a09e29204d8046fc
Gil Gardosh <gilgardosh@gmail.com>
2024-11-14 14:38:26 +0200

    .

--- packages/migrations/src/actions/2024-11-11T09-54-23.financial-accounts-enum-type.ts
+++ packages/migrations/src/actions/2024-11-11T09-54-23.financial-accounts-enum-type.ts
@@ -17,12 +17,15 @@ export default {
 
     CREATE TYPE accounter_schema.financial_account_type as enum ('BANK_ACCOUNT', 'CREDIT_CARD', 'CRYPTO_WALLET');
 
+    -- clean dependent views
+    DROP VIEW accounter_schema.extended_business_trip_transactions;
     DROP VIEW accounter_schema.extended_charges;
     DROP VIEW accounter_schema.extended_transactions;
 
     ALTER TABLE accounter_schema.financial_accounts
         ALTER COLUMN type TYPE accounter_schema.financial_account_type USING type::accounter_schema.financial_account_type;
 
+    -- restore dependent views
     create or replace view accounter_schema.extended_transactions
                 (id, charge_id, business_id, currency, debit_date, debit_timestamp, source_debit_date, event_date,
                 account_id, account_type, amount, current_balance, source_description, source_details, created_at,
@@ -397,5 +400,55 @@ export default {
             LEFT JOIN ledger_by_charge ON ledger_by_charge.charge_id = c.id
             LEFT JOIN years_of_relevance y ON y.charge_id = c.id
             LEFT JOIN accounter_schema.depreciation d ON d.charge_id = c.id;
+
+    create or replace view accounter_schema.extended_business_trip_transactions
+                (id, business_trip_id, transaction_ids, charge_ids, category, date, value_date, amount, currency,
+                employee_business_id, payed_by_employee)
+    as
+    WITH transactions_by_business_trip_transaction AS (SELECT tm.business_trip_transaction_id,
+                                                              array_agg(DISTINCT t1.id)        AS transaction_ids,
+                                                              array_agg(DISTINCT t1.charge_id) AS charge_ids,
+                                                              array_agg(DISTINCT t1.currency)  AS currencies,
+                                                              sum(tm.amount)                   AS amount,
+                                                              min(t1.event_date)               AS event_date,
+                                                              min(t1.debit_date)               AS debit_date
+                                                      FROM accounter_schema.business_trips_transactions_match tm
+                                                                LEFT JOIN accounter_schema.extended_transactions t1 ON t1.id = tm.transaction_id
+                                                      GROUP BY tm.business_trip_transaction_id)
+    SELECT DISTINCT ON (btt.id) btt.id,
+                                btt.business_trip_id,
+                                t.transaction_ids,
+                                t.charge_ids,
+                                btt.category,
+                                CASE
+                                    WHEN t.business_trip_transaction_id IS NULL THEN ep.date
+                                    ELSE t.event_date
+                                    END                                AS date,
+                                CASE
+                                    WHEN t.business_trip_transaction_id IS NULL THEN ep.value_date
+                                    ELSE t.debit_date
+                                    END                                AS value_date,
+                                CASE
+                                    WHEN t.business_trip_transaction_id IS NULL THEN
+                                        CASE
+                                            WHEN ep.amount IS NULL THEN NULL::numeric
+                                            ELSE ep.amount * '-1'::integer::numeric
+                                            END
+                                    WHEN array_length(t.currencies, 1) = 1 THEN t.amount
+                                    ELSE NULL::numeric
+                                    END                                AS amount,
+                                CASE
+                                    WHEN t.business_trip_transaction_id IS NULL THEN ep.currency
+                                    WHEN array_length(t.currencies, 1) = 1 THEN t.currencies[1]
+                                    ELSE NULL::accounter_schema.currency
+                                    END                                AS currency,
+                                ep.employee_business_id,
+                                t.business_trip_transaction_id IS NULL AS payed_by_employee
+    FROM accounter_schema.business_trips_transactions btt
+            LEFT JOIN transactions_by_business_trip_transaction t ON t.business_trip_transaction_id = btt.id
+            LEFT JOIN accounter_schema.business_trips_employee_payments ep ON ep.id = btt.id;
+
+    alter table accounter_schema.extended_business_trip_transactions
+        owner to accounter_prod_user;
 `,
 } satisfies MigrationExecutor;
