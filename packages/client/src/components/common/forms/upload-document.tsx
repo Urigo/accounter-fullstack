ba7449ae80970b82878a7dfb43d6e5c85afacf47
Gil Gardosh <gilgardosh@gmail.com>
2024-11-14 16:12:13 +0200

    Enhance client document upload impl (#1371)

--- packages/client/src/components/common/forms/upload-document.tsx
+++ packages/client/src/components/common/forms/upload-document.tsx
@@ -1,25 +1,6 @@
 import { ReactElement, useCallback, useState } from 'react';
-import { useMutation } from 'urql';
 import { FileInput, Loader } from '@mantine/core';
-import { showNotification } from '@mantine/notifications';
-import { CommonError, UploadDocumentDocument } from '../../../gql/graphql.js';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-expressions -- used by codegen
-/* GraphQL */ `
-  mutation UploadDocument($file: FileScalar!, $chargeId: UUID) {
-    uploadDocument(file: $file, chargeId: $chargeId) {
-      __typename
-      ... on UploadDocumentSuccessfulResult {
-        document {
-          id
-        }
-      }
-      ... on CommonError {
-        message
-      }
-    }
-  }
-`;
+import { useUploadDocument } from '../../../hooks/use-upload-document.js';
 
 type Props = {
   chargeId: string;
@@ -28,49 +9,23 @@ type Props = {
 };
 
 export const UploadDocument = ({ chargeId, closeModal, onChange }: Props): ReactElement => {
-  const [res, mutate] = useMutation(UploadDocumentDocument);
+  const { fetching, uploadDocument } = useUploadDocument();
   const [value, setValue] = useState<File | null>(null);
 
   const onSubmit = useCallback(async () => {
-    if (value) {
-      mutate({
-        file: value,
-        chargeId,
-      })
-        .then(res => {
-          if (res.error || res.data?.uploadDocument.__typename === 'CommonError') {
-            showNotification({
-              title: 'Error!',
-              message: 'Oh no!, we have an error! 🤥',
-            });
-            console.error(
-              `Error uploading document: ${
-                res.error?.message || (res.data?.uploadDocument as CommonError).message
-              }`,
-            );
-          } else if (res.data) {
-            onChange?.();
-            showNotification({
-              title: 'Upload Success!',
-              message: 'Hey there, you add new document!',
-            });
-            closeModal();
-          }
-        })
-        .catch(e => {
-          showNotification({
-            title: 'Error!',
-            message: 'Oh no!, we have an error! 🤥',
-          });
-          console.error(`Error uploading document: ${(e as Error)?.message}`);
-        });
-    }
-  }, [mutate, closeModal, onChange, value, chargeId]);
+    uploadDocument({
+      file: value,
+      chargeId,
+    }).then(() => {
+      onChange();
+      closeModal();
+    });
+  }, [uploadDocument, closeModal, onChange, value, chargeId]);
 
   return (
     <div className="px-5 w-max h-max justify-items-center">
       <FileInput
-        icon={res.fetching && <Loader />}
+        icon={fetching && <Loader />}
         value={value}
         onChange={setValue}
         clearable
@@ -80,7 +35,7 @@ export const UploadDocument = ({ chargeId, closeModal, onChange }: Props): React
         <button
           type="submit"
           className=" text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-          disabled={res.fetching || !value}
+          disabled={fetching || !value}
           onClick={onSubmit}
         >
           Accept
---
+++ packages/client/src/hooks/use-upload-document.ts
@@ -0,0 +1,89 @@
+import { useMutation } from 'urql';
+import { showNotification } from '@mantine/notifications';
+import {
+  UploadDocumentDocument,
+  UploadDocumentMutation,
+  UploadDocumentMutationVariables,
+} from '../gql/graphql.js';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions -- used by codegen
+/* GraphQL */ `
+  mutation UploadDocument($file: FileScalar!, $chargeId: UUID) {
+    uploadDocument(file: $file, chargeId: $chargeId) {
+      __typename
+      ... on UploadDocumentSuccessfulResult {
+        document {
+          id
+        }
+      }
+      ... on CommonError {
+        message
+      }
+    }
+  }
+`;
+
+type UploadDocumentSuccessfulResult = Extract<
+  UploadDocumentMutation['uploadDocument'],
+  { __typename: 'UploadDocumentSuccessfulResult' }
+>;
+
+type UseUploadDocument = {
+  fetching: boolean;
+  uploadDocument: (
+    variables: UploadDocumentMutationVariables,
+  ) => Promise<UploadDocumentSuccessfulResult>;
+};
+
+export const useUploadDocument = (): UseUploadDocument => {
+  // TODO: add authentication
+  // TODO: add local data update method after upload
+
+  const [{ fetching }, mutate] = useMutation(UploadDocumentDocument);
+
+  return {
+    fetching,
+    uploadDocument: (
+      variables: UploadDocumentMutationVariables,
+    ): Promise<UploadDocumentSuccessfulResult> =>
+      new Promise<UploadDocumentSuccessfulResult>((resolve, reject) =>
+        mutate(variables).then(res => {
+          if (res.error) {
+            console.error(
+              `Error uploading document to charge ID [${variables.chargeId}]: ${res.error}`,
+            );
+            showNotification({
+              title: 'Error!',
+              message: 'Oh no!, we have an error! 🤥',
+            });
+            return reject(res.error.message);
+          }
+          if (!res.data) {
+            console.error(
+              `Error uploading document to charge ID [${variables.chargeId}]: No data returned`,
+            );
+            showNotification({
+              title: 'Error!',
+              message: 'Oh no!, we have an error! 🤥',
+            });
+            return reject('No data returned');
+          }
+          if (res.data.uploadDocument.__typename === 'CommonError') {
+            console.error(
+              `Error uploading document to charge ID [${variables.chargeId}]: ${res.data.uploadDocument.message}`,
+            );
+            showNotification({
+              title: 'Error!',
+              message: 'Oh no!, we have an error! 🤥',
+            });
+            return reject(res.data.uploadDocument.message);
+          }
+          showNotification({
+            title: 'Upload Success!',
+            message: 'Your document was added! 🎉',
+          });
+          return resolve(res.data.uploadDocument);
+        }),
+      ),
+  };
+};
