5e13809f7c5672afb5b1651f62bef6b9b727deec
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-12-08 17:54:39 +0200

    Removed unused code

--- packages/modern-poalim-scraper/src/helpers/waiting.ts
+++ packages/modern-poalim-scraper/src/helpers/waiting.ts
@@ -1,8 +1,6 @@
 // From: https://github.com/eshaham/israeli-bank-scrapers/blob/master/src/helpers/waiting.ts
 
-export class TimeoutError extends Error {}
-
-export const SECOND = 1000;
+class TimeoutError extends Error {}
 
 function timeoutPromise<T>(ms: number, promise: Promise<T>, description: string): Promise<T> {
   const timeout = new Promise((_, reject) => {
@@ -46,21 +44,4 @@ export function waitUntil<T>(
     wait();
   });
   return timeoutPromise(timeout, promise, description);
-}
-
-export function raceTimeout(ms: number, promise: Promise<any>) {
-  return timeoutPromise(ms, promise, 'timeout').catch(err => {
-    if (!(err instanceof TimeoutError)) throw err;
-  });
-}
-
-export function runSerial<T>(actions: (() => Promise<T>)[]): Promise<T[]> {
-  return actions.reduce(
-    (m, a) => m.then(async x => [...x, await a()]),
-    Promise.resolve<T[]>(new Array<T>()),
-  );
-}
-
-export function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
