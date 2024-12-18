a5fc7e76f1a1396e61cb528f4535b5ffab56f15c
Gil Gardosh <gilgardosh@gmail.com>
2024-12-18 14:56:25 +0200

    lint

--- packages/client/src/components/common/new-documnts-list.tsx
+++ packages/client/src/components/common/new-documnts-list.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react';
 import { NavLink } from '@mantine/core';
-import { FragmentType, getFragmentData } from '../../gql';
-import { NewFetchedDocumentFieldsFragmentDoc } from '../../gql/graphql';
+import { NewFetchedDocumentFieldsFragmentDoc } from '../../gql/graphql.js';
+import { FragmentType, getFragmentData } from '../../gql/index.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-expressions -- used by codegen
 /* GraphQL */ `
