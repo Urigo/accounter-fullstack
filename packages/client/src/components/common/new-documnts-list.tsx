d975d8ad29574a270a6ae24bb47ba6adf9b54c75
Gil Gardosh <gilgardosh@gmail.com>
2024-12-18 15:11:54 +0200

    Enhancement: better notifications for fetching documents from green i… (#1516)
    * Enhancement: better notifications for fetching documents from green invoice
    
    * lint

---
+++ packages/client/src/components/common/new-documnts-list.tsx
@@ -0,0 +1,45 @@
+import { ReactElement } from 'react';
+import { NavLink } from '@mantine/core';
+import { NewFetchedDocumentFieldsFragmentDoc } from '../../gql/graphql.js';
+import { FragmentType, getFragmentData } from '../../gql/index.js';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions -- used by codegen
+/* GraphQL */ `
+  fragment NewFetchedDocumentFields on Document {
+    id
+    documentType
+    charge {
+      id
+      userDescription
+      counterparty {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export interface Props {
+  data: FragmentType<typeof NewFetchedDocumentFieldsFragmentDoc>[];
+}
+
+export const NewDocumentsList = ({ data }: Props): ReactElement => {
+  const documents = data.map(doc => getFragmentData(NewFetchedDocumentFieldsFragmentDoc, doc));
+
+  return (
+    <div className="flex flex-col gap-4 mt-5">
+      {documents
+        .filter(({ charge }) => charge)
+        .map(doc => (
+          <NavLink
+            key={doc.id}
+            label={`${doc.charge!.counterparty?.name ?? 'Unknown'} - ${doc.charge!.userDescription}`}
+            onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+              event.stopPropagation();
+              window.open(`/charges/${doc.charge!.id}`, '_blank', 'noreferrer');
+            }}
+          />
+        ))}
+    </div>
+  );
+};
