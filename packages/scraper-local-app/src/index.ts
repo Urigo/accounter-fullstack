85cfd143a985b4dd8338535c2443b8ccc2e5ed8e
Gil Gardosh <gilgardosh@gmail.com>
2025-01-02 08:55:29 +0200

    main status update for creditcards

--- packages/scraper-local-app/src/index.ts
+++ packages/scraper-local-app/src/index.ts
@@ -2,9 +2,9 @@ import Listr, { ListrTask } from 'listr';
 import pg from 'pg';
 import { config } from './env.js';
 import { Logger } from './logger.js';
-// import { getCurrencyRates } from './scrapers/currency-rates.js';
-// import { getPoalimData } from './scrapers/poalim/index.js';
+import { getCurrencyRates } from './scrapers/currency-rates.js';
 import { getIsracardAmexData } from './scrapers/isracard-amex/index.js';
+import { getPoalimData } from './scrapers/poalim/index.js';
 
 const { Pool } = pg;
 type Context = { logger: Logger };
@@ -16,29 +16,31 @@ export async function scrape() {
 
   const tasks = new Listr<Context>(
     [
-      // {
-      //   title: 'Currency Rates',
-      //   task: async ctx => getCurrencyRates(pool, ctx.logger),
-      // },
-      // ...(config.poalimAccounts?.map(
-      //   (creds, i) =>
-      //     ({
-      //       title: `Poalim Account ${creds.nickname ?? i + 1}`,
-      //       task: async ctx => await getPoalimData(pool, creds, ctx.logger),
-      //     }) as ListrTask<Context>,
-      // ) ?? []),
-      // ...(config.isracardAccounts?.map(
-      //   (creds, i) =>
-      //     ({
-      //       title: `Isracard ${creds.nickname ?? i + 1}`,
-      //       task: async ctx => await getIsracardAmexData('ISRACARD', pool, creds, ctx.logger),
-      //     }) as ListrTask<Context>,
-      // ) ?? []),
+      {
+        title: 'Currency Rates',
+        task: async ctx => getCurrencyRates(pool, ctx.logger),
+      },
+      ...(config.poalimAccounts?.map(
+        (creds, i) =>
+          ({
+            title: `Poalim Account ${creds.nickname ?? i + 1}`,
+            task: async ctx => await getPoalimData(pool, creds, ctx.logger),
+          }) as ListrTask<Context>,
+      ) ?? []),
+      ...(config.isracardAccounts?.map(
+        (creds, i) =>
+          ({
+            title: `Isracard ${creds.nickname ?? i + 1}`,
+            task: async (ctx, task) =>
+              await getIsracardAmexData('ISRACARD', pool, creds, ctx.logger, task),
+          }) as ListrTask<Context>,
+      ) ?? []),
       ...(config.amexAccounts?.map(
         (creds, i) =>
           ({
             title: `American Express ${creds.nickname ?? i + 1}`,
-            task: async ctx => await getIsracardAmexData('AMEX', pool, creds, ctx.logger),
+            task: async (ctx, task) =>
+              await getIsracardAmexData('AMEX', pool, creds, ctx.logger, task),
           }) as ListrTask<Context>,
       ) ?? []),
     ],
