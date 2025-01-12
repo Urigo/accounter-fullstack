880d5ddbdc8d65728e084920af732e9fb9a8bdd2
Gil Gardosh <gilgardosh@gmail.com>
2025-01-12 11:50:19 +0200

    DB migration

--- packages/migrations/src/run-pg-migrations.ts
+++ packages/migrations/src/run-pg-migrations.ts
@@ -66,6 +66,7 @@ import migration_2024_12_16T09_31_25_amex_scraper_completion from './actions/202
 import migration_2024_12_20T08_23_25_use_businesses_for_foreign_keys from './actions/2024-12-20T08-23-25.use-businesses-for-foreign-keys.js';
 import migration_2025_01_05T18_20_42_vat_table from './actions/2025-01-05T18-20-42.vat-table.js';
 import migration_2025_01_07T18_54_54_poalim_foreign_event_details_column_update from './actions/2025-01-07T18-54-54.poalim-foreign-event-details-column-update.js';
+import migration_2025_01_12T11_47_46_support_cad_currency from './actions/2025-01-12T11-47-46.support-cad-currency.js';
 import { runMigrations } from './pg-migrator.js';
 
 export const runPGMigrations = (args: { slonik: DatabasePool }) =>
@@ -139,5 +140,6 @@ export const runPGMigrations = (args: { slonik: DatabasePool }) =>
       migration_2024_12_20T08_23_25_use_businesses_for_foreign_keys,
       migration_2025_01_05T18_20_42_vat_table,
       migration_2025_01_07T18_54_54_poalim_foreign_event_details_column_update,
+      migration_2025_01_12T11_47_46_support_cad_currency,
     ],
   });
