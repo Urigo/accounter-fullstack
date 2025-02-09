e7e6d8a3748a9945338a5c21010f5288e46667d7
Gil Gardosh <gilgardosh@gmail.com>
2025-02-09 17:20:23 +0200

    Adjust reports to include new sorting codes

--- packages/server/src/modules/reports/resolvers/reports/corporate-tax-ruling-compliance-report.ts
+++ packages/server/src/modules/reports/resolvers/reports/corporate-tax-ruling-compliance-report.ts
@@ -37,7 +37,7 @@ function rndTaxCategoryType(sortCode?: number | null): ExpenseType | null {
   if (!sortCode) {
     return null;
   }
-  if (sortCode === 920) {
+  if ([920, 922, 923, 924].includes(sortCode)) {
     // R&D expenses
     return ExpenseType.DevOps;
   }
