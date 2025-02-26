f9a8bd7160ba2e399d917029b35db074966ff0d9
Gil Gardosh <gilgardosh@gmail.com>
2025-02-26 09:59:07 +0200

    Enhance corporate ruling report: add summary column (#1812)

--- packages/client/src/components/reports/corporate-tax-ruling-compliance-report/amount-cell.tsx
+++ packages/client/src/components/reports/corporate-tax-ruling-compliance-report/amount-cell.tsx
@@ -1,40 +1,20 @@
 import { ReactElement } from 'react';
-import { FragmentType, getFragmentData } from '../../../gql/fragment-masking.js';
-import { CorporateTaxRulingReportAmountCellFieldsFragmentDoc } from '../../../gql/graphql.js';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-expressions -- used by codegen
-/* GraphQL */ `
-  fragment CorporateTaxRulingReportAmountCellFields on FinancialAmount {
-    formatted
-  }
-`;
 
 type Props = {
-  originalAmountData: FragmentType<typeof CorporateTaxRulingReportAmountCellFieldsFragmentDoc>;
-  diffAmountData?: FragmentType<typeof CorporateTaxRulingReportAmountCellFieldsFragmentDoc>;
+  originalAmount: string;
+  diffAmount?: string;
 };
 
-export const AmountCell = ({ originalAmountData, diffAmountData }: Props): ReactElement => {
-  const originalAmount = getFragmentData(
-    CorporateTaxRulingReportAmountCellFieldsFragmentDoc,
-    originalAmountData,
-  );
-  const diffAmount = getFragmentData(
-    CorporateTaxRulingReportAmountCellFieldsFragmentDoc,
-    diffAmountData,
-  );
-
-  if (!diffAmount) {
-    return <td>{originalAmount.formatted}</td>;
+export const AmountCell = ({ originalAmount, diffAmount }: Props): ReactElement => {
+  if (!diffAmount || originalAmount === diffAmount) {
+    return <td>{originalAmount}</td>;
   }
 
   return (
     <td>
       <div className="flex flex-col">
-        <p className={diffAmount ? 'line-through' : ''}>{originalAmount.formatted}</p>
-        {diffAmount && (
-          <div className="border-2 border-yellow-500 rounded-md">{diffAmount?.formatted}</div>
-        )}
+        <p className={diffAmount ? 'line-through' : ''}>{originalAmount}</p>
+        {diffAmount && <div className="border-2 border-yellow-500 rounded-md">{diffAmount}</div>}
       </div>
     </td>
   );
