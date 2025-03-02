3b7b5e500d33a4074a0df08039520f92366c5bfa
TuvalSimha <tuval.simha@gmail.com>
2025-03-02 11:52:51 +0200

    fix?

--- packages/gateway/index.ts
+++ packages/gateway/index.ts
@@ -1,9 +1,14 @@
 import { createServer } from 'node:http';
 import { createGatewayRuntime } from '@graphql-hive/gateway-runtime';
 import http from '@graphql-mesh/transport-http';
+import { env } from '../server/src/environment';
 
 const gateway = createGatewayRuntime({
-  supergraph: '../../supergraph.graphql',
+  supergraph: {
+    type: 'hive',
+    endpoint: 'https://cdn.graphql-hive.com/artifacts/v1/1766c3d3-f0ba-46ff-8252-1ec116d07123',
+    key: env.hive.hiveMainRegistryToken,
+  },
   transports: { http },
 });
 const server = createServer(gateway);
--- packages/server/src/environment.ts
+++ packages/server/src/environment.ts
@@ -60,6 +60,9 @@ const HiveModel = zod.union([
     HIVE_TOKEN: zod.string().optional(),
     HIVE_GATEWAY_PORT: NumberFromString.optional(),
     HIVE_SUBGRAPH_PORT: NumberFromString.optional(),
+    HIVE_DEV_REGISTRY_TOKEN: zod.string().optional(),
+    HIVE_MAIN_REGISTRY_TOKEN: zod.string(),
+    HIVE_STAGING_REGISTRY_TOKEN: zod.string().optional(),
   }),
   zod.void(),
 ]);
@@ -134,6 +137,9 @@ export const env = {
     hiveToken: hive?.HIVE_TOKEN,
     hiveGatewayPort: hive?.HIVE_GATEWAY_PORT ?? 4000,
     hiveSubgraphPort: hive?.HIVE_SUBGRAPH_PORT ?? 4001,
+    hiveDevRegistryToken: hive?.HIVE_DEV_REGISTRY_TOKEN,
+    hiveMainRegistryToken: hive?.HIVE_MAIN_REGISTRY_TOKEN ?? '',
+    hiveStagingRegistryToken: hive?.HIVE_STAGING_REGISTRY_TOKEN,
   },
   google: {
     driveApiKey: google?.GOOGLE_DRIVE_API_KEY,
