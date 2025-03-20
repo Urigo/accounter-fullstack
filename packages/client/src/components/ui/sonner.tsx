66e66f853b63734dd6338b179fab628faba0ae3f
Gil Gardosh <gilgardosh@gmail.com>
2025-03-20 10:07:15 +0200

    minor UI fix (notification theme)

--- packages/client/src/components/ui/sonner.tsx
+++ packages/client/src/components/ui/sonner.tsx
@@ -2,7 +2,7 @@ import { useTheme } from 'next-themes';
 import { Toaster as Sonner, ToasterProps } from 'sonner';
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = 'system' } = useTheme();
+  const { theme = 'ligh' } = useTheme();
 
   return (
     <Sonner
