b8ed84fcc3898cb02b60ee61610a63ae5e52dae9
Gil Gardosh <gilgardosh@gmail.com>
2025-03-21 14:37:40 +0200

    Migrate ledger table into data-table (#1941)
    * Migrate ledger table into data-table
    
    * Update packages/client/src/components/charges/ledger-records/columns.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * better uri handle
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ packages/client/src/components/charges/ledger-records/date-cell.tsx
@@ -0,0 +1,26 @@
+import { ReactElement } from 'react';
+import { format } from 'date-fns';
+
+type Props = {
+  date?: Date | null;
+  diff?: Date | null;
+};
+
+export const DateCell = ({ date, diff }: Props): ReactElement => {
+  const formattedDate = date ? format(new Date(date), 'dd/MM/yy') : undefined;
+
+  // calculate diff date
+  const diffFormattedDate = diff ? format(new Date(diff), 'dd/MM/yy') : undefined;
+  const isDiff = diffFormattedDate && formattedDate !== diffFormattedDate;
+
+  return (
+    <td>
+      <p className={isDiff ? 'line-through' : ''}>{formattedDate ?? 'Missing Data'}</p>
+      {isDiff && (
+        <div className="flex flex-col border-2 border-yellow-500 rounded-md">
+          {diffFormattedDate}
+        </div>
+      )}
+    </td>
+  );
+};
