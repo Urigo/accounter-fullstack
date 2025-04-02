0e27e261c61637699e92493e71b0e2a6d3c90d14
Gil Gardosh <gilgardosh@gmail.com>
2025-04-02 10:29:38 +0300

    issue docs form

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -23,6 +23,7 @@ import { AllCharges } from './components/screens/charges/all-charges.js';
 import { Charge } from './components/screens/charges/charge.js';
 import { MissingInfoCharges } from './components/screens/charges/missing-info-charges.js';
 import { DocumentsReport } from './components/screens/documents/all-documents/index.jsx';
+import { IssueDocuments } from './components/screens/documents/issue-documents/index.js';
 import { BalanceReport } from './components/screens/reports/balance-report.js';
 import { TagsManager } from './components/tags/index.js';
 import { TaxCategories } from './components/tax-categories/index.js';
@@ -53,6 +54,7 @@ export function App(): ReactElement {
             <Route path="monthly-income-expense" element={<MonthlyIncomeExpenseChart />} />
           </Route>
           <Route path="documents" element={<DocumentsReport />} />
+          <Route path="issue-documents" element={<IssueDocuments />} />
           <Route path="accountant-approvals" element={<AccountantApprovals />} />
           <Route path="reports">
             <Route path="trial-balance" element={<TrialBalanceReport />} />
--- packages/client/src/components/common/inputs/number-input.tsx
+++ packages/client/src/components/common/inputs/number-input.tsx
@@ -1,10 +1,143 @@
-import { ComponentProps, forwardRef, ReactElement } from 'react';
-import { Input } from './input.js';
+import { forwardRef, useCallback, useEffect, useState } from 'react';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import { NumericFormat, NumericFormatProps } from 'react-number-format';
+import { Button } from '../../ui/button';
+import { Input } from '../../ui/input';
 
-type Props = Omit<ComponentProps<typeof Input>, 'type'> & {
-  children?: ReactElement | ReactElement[];
-};
+export interface NumberInputProps extends Omit<NumericFormatProps, 'value' | 'onValueChange'> {
+  stepper?: number;
+  thousandSeparator?: string;
+  placeholder?: string;
+  defaultValue?: number;
+  min?: number;
+  max?: number;
+  value?: number; // Controlled value
+  suffix?: string;
+  prefix?: string;
+  onValueChange?: (value: number | undefined) => void;
+  fixedDecimalScale?: boolean;
+  decimalScale?: number;
+  hideControls?: boolean;
+}
 
-export const NumberInput = forwardRef<HTMLInputElement, Props>(function NumberInput(props) {
-  return <Input {...props} type="number" step="any" />;
+export const NumberInput = forwardRef<HTMLInputElement, NumberInputProps>(function NumberInput(
+  {
+    stepper,
+    thousandSeparator,
+    placeholder,
+    defaultValue,
+    min = -Infinity,
+    max = Infinity,
+    onValueChange,
+    fixedDecimalScale = false,
+    decimalScale = 0,
+    suffix,
+    prefix,
+    value: controlledValue,
+    hideControls = false,
+    ...props
+  },
+  ref,
+) {
+  const [value, setValue] = useState<number | undefined>(controlledValue ?? defaultValue);
+  console.log('number value', value);
+
+  const handleIncrement = useCallback(() => {
+    setValue(prev => (prev === undefined ? (stepper ?? 1) : Math.min(prev + (stepper ?? 1), max)));
+  }, [stepper, max]);
+
+  const handleDecrement = useCallback(() => {
+    setValue(prev => (prev === undefined ? -(stepper ?? 1) : Math.max(prev - (stepper ?? 1), min)));
+  }, [stepper, min]);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (document.activeElement === (ref as React.RefObject<HTMLInputElement>).current) {
+        if (e.key === 'ArrowUp') {
+          handleIncrement();
+        } else if (e.key === 'ArrowDown') {
+          handleDecrement();
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleIncrement, handleDecrement, ref]);
+
+  useEffect(() => {
+    if (controlledValue !== undefined) {
+      setValue(controlledValue);
+    }
+  }, [controlledValue]);
+
+  const handleChange = (values: { value: string; floatValue: number | undefined }) => {
+    const newValue = values.floatValue === undefined ? undefined : values.floatValue;
+    setValue(newValue);
+    if (onValueChange) {
+      onValueChange(newValue);
+    }
+  };
+
+  const handleBlur = () => {
+    if (value !== undefined) {
+      if (value < min) {
+        setValue(min);
+        (ref as React.RefObject<HTMLInputElement>).current!.value = String(min);
+      } else if (value > max) {
+        setValue(max);
+        (ref as React.RefObject<HTMLInputElement>).current!.value = String(max);
+      }
+    }
+  };
+
+  return (
+    <div className="flex items-center">
+      <NumericFormat
+        value={value}
+        onValueChange={handleChange}
+        thousandSeparator={thousandSeparator}
+        decimalScale={decimalScale}
+        fixedDecimalScale={fixedDecimalScale}
+        allowNegative={min < 0}
+        valueIsNumericString
+        onBlur={handleBlur}
+        max={max}
+        min={min}
+        suffix={suffix}
+        prefix={prefix}
+        customInput={Input}
+        placeholder={placeholder}
+        className={`[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none relative ${hideControls ? '' : ' rounded-r-none'}`}
+        getInputRef={ref}
+        {...props}
+      />
+
+      {hideControls ? null : (
+        <div className="flex flex-col">
+          <Button
+            aria-label="Increase value"
+            className="px-2 h-5 rounded-l-none rounded-br-none border-input border-l-0 border-b-[0.5px] focus-visible:relative"
+            variant="outline"
+            onClick={handleIncrement}
+            disabled={value === max}
+          >
+            <ChevronUp size={15} />
+          </Button>
+          <Button
+            aria-label="Decrease value"
+            className="px-2 h-5 rounded-l-none rounded-tr-none border-input border-l-0 border-t-[0.5px] focus-visible:relative"
+            variant="outline"
+            onClick={handleDecrement}
+            disabled={value === min}
+          >
+            <ChevronDown size={15} />
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 });
