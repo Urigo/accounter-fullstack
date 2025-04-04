13392e9093c32b946145e066878ebe2120763067
Gil Gardosh <gilgardosh@gmail.com>
2025-04-04 14:34:57 +0300

    cleanups

--- packages/client/src/components/transactions-table/cells-legacy/index.ts
+++ packages/client/src/components/transactions-table/cells-legacy/index.ts
@@ -1,6 +1,5 @@
 export { DebitDate } from './debit-date.jsx';
 export { EventDate } from './event-date.jsx';
-export { Amount } from './amount.jsx';
 export { Account } from './account.jsx';
 export { Description } from './description.jsx';
 export { SourceID } from './source-id.jsx';
