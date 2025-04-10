73e03b696b73111cd38b5730ffb384ce97db8644
Gil Gardosh <gilgardosh@gmail.com>
2025-04-10 15:40:43 +0300

    Fix: credit card charge related transactions tab

--- packages/client/src/components/charges/extended-info/creditcard-transactions-info.tsx
+++ packages/client/src/components/charges/extended-info/creditcard-transactions-info.tsx
@@ -1,15 +1,29 @@
-import { ReactElement } from 'react';
-import { CreditcardBankChargeInfoFragmentDoc } from '../../../gql/graphql.js';
+import { ReactNode, useMemo } from 'react';
+import {
+  CreditcardBankChargeInfoFragmentDoc,
+  TransactionForTransactionsTableFieldsFragmentDoc,
+} from '../../../gql/graphql.js';
 import { FragmentType, getFragmentData } from '../../../gql/index.js';
-import { TransactionsTable } from '../../transactions-table/index.js';
+import { ChargeNavigateButton } from '../../common/index.js';
+import {
+  Account,
+  Amount,
+  Counterparty,
+  DebitDate,
+  Description,
+  EventDate,
+  SourceID,
+} from '../../transactions-table/cells/index.js';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../ui/table.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-expressions -- used by codegen
 /* GraphQL */ `
   fragment CreditcardBankChargeInfo on Charge {
     id
     __typename
-    ... on CreditcardBankCharge @defer {
+    ... on CreditcardBankCharge {
       creditCardTransactions {
+        id
         ...TransactionForTransactionsTableFields
       }
     }
@@ -20,16 +34,73 @@ type Props = {
   chargeProps: FragmentType<typeof CreditcardBankChargeInfoFragmentDoc>;
 };
 
-export const CreditcardTransactionsInfo = ({ chargeProps }: Props): ReactElement => {
+export const CreditcardTransactionsInfo = ({ chargeProps }: Props): ReactNode => {
   const charge = getFragmentData(CreditcardBankChargeInfoFragmentDoc, chargeProps);
-  if (charge.__typename !== 'CreditcardBankCharge' || charge.creditCardTransactions?.length === 0) {
-    // eslint-disable-next-line react/jsx-no-useless-fragment
-    return <></>;
-  }
+  const hasTransactions =
+    charge.__typename === 'CreditcardBankCharge' && !!charge.creditCardTransactions;
+  const transactions = useMemo(() => {
+    if (charge.__typename === 'CreditcardBankCharge' && charge.creditCardTransactions) {
+      return charge.creditCardTransactions.map(rawTransaction =>
+        getFragmentData(TransactionForTransactionsTableFieldsFragmentDoc, rawTransaction),
+      );
+    }
+    return [];
+  }, [charge]);
 
-  return (
-    charge.creditCardTransactions && (
-      <TransactionsTable transactionsProps={charge.creditCardTransactions} enableChargeLink />
-    )
-  );
+  console.log('hasTransactions', hasTransactions);
+  return transactions.length ? (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Event Date</TableHead>
+          <TableHead>Debit Date</TableHead>
+          <TableHead>Amount</TableHead>
+          <TableHead>Account</TableHead>
+          <TableHead>Description</TableHead>
+          <TableHead>Reference#</TableHead>
+          <TableHead>Counterparty</TableHead>
+          <TableHead>Charge</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {transactions.map(transaction => {
+          const extendedTransaction = {
+            ...transaction,
+            onUpdate: () => void 0,
+            editTransaction: () => void 0,
+            enableEdit: false,
+            enableChargeLink: false,
+          };
+          return (
+            <TableRow key={transaction.id}>
+              <TableCell>
+                <EventDate transaction={extendedTransaction} />
+              </TableCell>
+              <TableCell>
+                <DebitDate transaction={extendedTransaction} />
+              </TableCell>
+              <TableCell>
+                <Amount transaction={extendedTransaction} />
+              </TableCell>
+              <TableCell>
+                <Account transaction={extendedTransaction} />
+              </TableCell>
+              <TableCell>
+                <Description transaction={extendedTransaction} />
+              </TableCell>
+              <TableCell>
+                <SourceID transaction={extendedTransaction} />
+              </TableCell>
+              <TableCell>
+                <Counterparty transaction={extendedTransaction} />
+              </TableCell>
+              <TableCell>
+                <ChargeNavigateButton chargeId={transaction.chargeId} />
+              </TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  ) : null;
 };
