10bef418a0ed87abc1147b7e2e049ff0b83b74c9
Gil Gardosh <gilgardosh@gmail.com>
2025-04-28 18:14:38 +0300

    clear filter context

--- packages/client/src/components/screens/documents/issue-documents/index.tsx
+++ packages/client/src/components/screens/documents/issue-documents/index.tsx
@@ -1,6 +1,7 @@
-import { ReactElement } from 'react';
+import { ReactElement, useContext } from 'react';
 import { useQuery } from 'urql';
 import { AllGreenInvoiceBusinessesDocument } from '../../../../gql/graphql.js';
+import { FiltersContext } from '../../../../providers/filters-context.js';
 import { AccounterLoader } from '../../../common/loader.js';
 import { IssueDocumentsTable } from './issue-documents-table.js';
 
@@ -22,9 +23,12 @@ import { IssueDocumentsTable } from './issue-documents-table.js';
 `;
 
 export const IssueDocuments = (): ReactElement => {
+  const { setFiltersContext } = useContext(FiltersContext);
   const [{ data, fetching }] = useQuery({
     query: AllGreenInvoiceBusinessesDocument,
   });
+
+  setFiltersContext(null);
 
   return fetching ? (
     <AccounterLoader />
