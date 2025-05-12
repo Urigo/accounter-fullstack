9d218cbdc2a7e85e505ab725ee6dc516e60f1c3c
Gil Gardosh <gilgardosh@gmail.com>
2025-05-12 14:05:57 +0300

    add file handle util

---
+++ packages/shaam6111-generator/src/utils/file-utils.ts
@@ -0,0 +1,72 @@
+import { generateReport, generateReportString } from '../generators/generate-report.js';
+import { parseReport, parseReportString } from '../parsers/index.js';
+import { ReportData } from '../types/index.js';
+import { validateReport } from '../validation/index.js';
+import { fromWindows1255, toWindows1255 } from './encoding.js';
+
+/**
+ * Converts a SHAAM6111 report to a file with proper Windows-1255 encoding.
+ * @param report The report string or ReportData object to convert.
+ * @param fileName The name of the file to create.
+ * @param validate If true, validates the report before converting.
+ * @returns A File object containing the encoded report.
+ */
+export function convertReportToFile(
+  report: string | ReportData,
+  fileName: string = 'SHAAM6111.dat',
+  validate: boolean = true,
+): File {
+  // If report is a ReportData object, convert it to a string
+  let reportString = '';
+  if (typeof report === 'string') {
+    if (validate) {
+      validateReport(report);
+    }
+    reportString = report;
+  } else {
+    reportString = validate ? generateReport(report) : generateReportString(report);
+  }
+
+  // Convert the report string to Windows-1255 encoding
+  const encodedReport = toWindows1255(reportString);
+
+  // Create and return a File object with .dat extension
+  return new File([encodedReport], fileName, {
+    type: 'application/octet-stream',
+  });
+}
+
+/**
+ * Reads a SHAAM6111 report from a file with proper Windows-1255 decoding.
+ * @param reportFile The file to read the report from.
+ * @param parseToObject If true, parses the report into a ReportData object; if false, returns the raw report string.
+ * @param validate If true, validates the report after decoding.
+ * @returns A Promise that resolves to either the raw report string or a parsed ReportData object.
+ */
+export async function readReportFromFile(
+  reportFile: File,
+  parseToObject: boolean = false,
+  validate: boolean = true,
+): Promise<string | ReportData> {
+  // Decode the buffer from Windows-1255 encoding
+  const arrayBuffer = await reportFile.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  const reportString = fromWindows1255(buffer);
+
+  // Return either the raw string or a parsed ReportData object
+  if (parseToObject) {
+    if (validate) {
+      return parseReport(reportString);
+    }
+
+    return parseReportString(reportString);
+  }
+
+  // If validate is true, validate the report string
+  if (validate) {
+    validateReport(reportString);
+  }
+
+  // Return the raw string;
+  return reportString;
+}
