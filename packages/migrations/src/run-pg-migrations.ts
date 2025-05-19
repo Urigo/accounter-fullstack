3d136e52ac9bb740cd9480a26155a1cbd482d9ea
Gil Gardosh <gilgardosh@gmail.com>
2025-05-19 15:59:27 +0300

    add designated DB column

--- packages/migrations/src/run-pg-migrations.ts
+++ packages/migrations/src/run-pg-migrations.ts
@@ -91,6 +91,7 @@ import migration_2025_04_10T15_44_07_tax_excluded_flag from './actions/2025-04-1
 import migration_2025_04_15T17_27_28_pcn874 from './actions/2025-04-15T17-27-28.pcn874.js';
 import migration_2025_05_13T12_28_00_corporate_special_tax_rate from './actions/2025-05-13T12-28-00.corporate-special-tax-rate.js';
 import migration_2025_05_18T13_02_20_override_documents_exchange_rate from './actions/2025-05-18T13-02-20.override-documents-exchange-rate.js';
+import migration_2025_05_19T15_45_46_override_pcn874_record_type from './actions/2025-05-19T15-45-46.override-pcn874-record-type.js';
 import { runMigrations } from './pg-migrator.js';
 
 export const runPGMigrations = (args: { slonik: DatabasePool }) =>
@@ -189,5 +190,6 @@ export const runPGMigrations = (args: { slonik: DatabasePool }) =>
       migration_2025_04_15T17_27_28_pcn874,
       migration_2025_05_13T12_28_00_corporate_special_tax_rate,
       migration_2025_05_18T13_02_20_override_documents_exchange_rate,
+      migration_2025_05_19T15_45_46_override_pcn874_record_type,
     ],
   });
