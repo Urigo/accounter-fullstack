d36405f406cfebbf5dad9bd17d81a2c80450e2d1
Gil Gardosh <gilgardosh@gmail.com>
2025-05-27 11:46:13 +0300

    Post-array-conversion update (#2170)

---
+++ packages/migrations/src/actions/2025-05-26T12-23-36.convert-irs-code-column-to-array2.ts
@@ -0,0 +1,12 @@
+import { type MigrationExecutor } from '../pg-migrator.js';
+
+export default {
+  name: '2025-05-26T12-23-36.convert-irs-code-column-to-array.sql',
+  run: ({ sql }) => sql`
+alter table accounter_schema.sort_codes
+    drop column default_irs_code;
+
+alter table accounter_schema.financial_entities
+    drop column irs_code;
+`,
+} satisfies MigrationExecutor;
