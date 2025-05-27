69da78a7f6a5524ab821187aa1b5a922d4fac7bc
Gil Gardosh <gilgardosh@gmail.com>
2025-05-27 11:47:46 +0300

    .

--- packages/migrations/src/actions/2025-05-26T12-23-36.convert-irs-code-column-to-array2.ts
+++ packages/migrations/src/actions/2025-05-26T12-23-36.convert-irs-code-column-to-array2.ts
@@ -1,7 +1,7 @@
 import { type MigrationExecutor } from '../pg-migrator.js';
 
 export default {
-  name: '2025-05-26T12-23-36.convert-irs-code-column-to-array.sql',
+  name: '2025-05-26T12-23-36.convert-irs-code-column-to-array2.sql',
   run: ({ sql }) => sql`
 alter table accounter_schema.sort_codes
     drop column default_irs_code;
