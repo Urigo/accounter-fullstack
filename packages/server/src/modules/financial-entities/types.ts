02a5bbe69fdac3b3ced66c82bf21810b9e9ab530
Gil Gardosh <gilgardosh@gmail.com>
2025-06-10 16:54:06 +0300

    .

--- packages/server/src/modules/financial-entities/types.ts
+++ packages/server/src/modules/financial-entities/types.ts
@@ -2,4 +2,3 @@ export * from './__generated__/types.js';
 export * from './__generated__/businesses.types.js';
 export * from './__generated__/financial-entities.types.js';
 export * from './__generated__/tax-categories.types.js';
-export type { numberArray } from './__generated__/financial-entities.types.js';
