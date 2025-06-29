17dc6c6719a8dfcf5243344fb40166aa5ed6d7c2
Gil Gardosh <gilgardosh@gmail.com>
2025-06-29 13:53:47 +0300

    Fix: Poalim swift insert trigger function (#2220)
    * Fix: Poalim swift insert trigger function
    
    * update
    
    * .
    
    * fixes

--- packages/migrations/src/run-pg-migrations.ts
+++ packages/migrations/src/run-pg-migrations.ts
@@ -99,6 +99,7 @@ import migration_2025_06_08T12_23_53_poalim_foreign_less_strict from './actions/
 import migration_2025_06_09T18_11_34_convert_irs_codes_array_column_to_single from './actions/2025-06-09T18-11-34.convert-irs-codes-array-column-to-single.js';
 import migration_2025_06_10T10_45_37_convert_irs_codes_array_column_to_single2 from './actions/2025-06-10T10-45-37.convert-irs-codes-array-column-to-single2.js';
 import migration_2025_06_26T20_52_26_migrate_from_multiple_tables_to_single_poalim_foreign_table from './actions/2025-06-26T20-52-26.migrate-from-multiple-tables-to-single-poalim-foreign-table.js';
+import migration_2025_06_28T13_12_45_fix_poalim_swift_trigger_function from './actions/2025-06-28T13-12-45.fix-poalim-swift-trigger-function.js';
 import { runMigrations } from './pg-migrator.js';
 
 export const runPGMigrations = (args: { slonik: DatabasePool }) =>
@@ -205,5 +206,6 @@ export const runPGMigrations = (args: { slonik: DatabasePool }) =>
       migration_2025_06_09T18_11_34_convert_irs_codes_array_column_to_single,
       migration_2025_06_10T10_45_37_convert_irs_codes_array_column_to_single2,
       migration_2025_06_26T20_52_26_migrate_from_multiple_tables_to_single_poalim_foreign_table,
+      migration_2025_06_28T13_12_45_fix_poalim_swift_trigger_function,
     ],
   });
