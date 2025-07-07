060af05074535fd1af2f873deccbf49ac729ff44
Gil Gardosh <gilgardosh@gmail.com>
2025-07-07 17:43:25 +0300

    non-related minor enhancements

--- packages/server/src/modules/depreciation/types.ts
+++ packages/server/src/modules/depreciation/types.ts
@@ -1,19 +1,4 @@
 export * from './__generated__/types.js';
+export type { NumberOrString } from './__generated__/depreciation.types.js';
 export * from './__generated__/depreciation.types.js';
-export type {
-  IGetAllDepreciationCategoriesParams,
-  IGetAllDepreciationCategoriesResult,
-  IGetAllDepreciationCategoriesQuery,
-  IGetDepreciationCategoriesByIdsParams,
-  IGetDepreciationCategoriesByIdsResult,
-  IGetDepreciationCategoriesByIdsQuery,
-  IUpdateDepreciationCategoryParams,
-  IUpdateDepreciationCategoryResult,
-  IUpdateDepreciationCategoryQuery,
-  IInsertDepreciationCategoryParams,
-  IInsertDepreciationCategoryResult,
-  IInsertDepreciationCategoryQuery,
-  IDeleteDepreciationCategoryParams,
-  IDeleteDepreciationCategoryResult,
-  IDeleteDepreciationCategoryQuery,
-} from './__generated__/depreciation-categories.types.js';
+export * from './__generated__/depreciation-categories.types.js';
