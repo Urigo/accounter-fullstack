0efba3cda6762988cbb4d15175ae3e286972d777
Gil Gardosh <gilgardosh@gmail.com>
2025-07-14 17:32:38 +0300

    rename folder

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -25,7 +25,7 @@ import { MissingInfoCharges } from './components/screens/charges/missing-info-ch
 import { DocumentsReport } from './components/screens/documents/all-documents/index.jsx';
 import { IssueDocumentScreen } from './components/screens/documents/issue-document.js';
 import { IssueDocuments } from './components/screens/documents/issue-documents/index.js';
-import { AnnualAuditFlow } from './components/screens/operations/year-end-audit/index.jsx';
+import { AnnualAuditFlow } from './components/screens/operations/annual-audit/index.jsx';
 import { BalanceReport } from './components/screens/reports/balance-report/index.js';
 import { DepreciationReport } from './components/screens/reports/depreciation-report/index.js';
 import { Shaam6111Report } from './components/screens/reports/shaam6111-report/index.js';
