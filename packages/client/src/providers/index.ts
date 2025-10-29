6b4f8bc975a23de63429b68d0806022dbfc5b226
Gil Gardosh <gilgardosh@gmail.com>
2025-10-29 14:39:52 +0200

    Cleanup providers

---
+++ packages/client/src/providers/index.ts
@@ -0,0 +1,4 @@
+export { AuthContext, AuthGuard, AuthProvider } from './auth-guard.jsx';
+export { UrqlProvider } from './urql.jsx';
+export { UserContext, UserProvider, type UserInfo } from './user-provider.jsx';
+export { FiltersContext } from './filters-context.js';
