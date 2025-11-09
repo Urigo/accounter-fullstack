5000f3176aca427dec94465bd58930b82188a8aa
Gil Gardosh <gilgardosh@gmail.com>
2025-11-09 18:26:20 +0200

    implement account editing

--- packages/server/src/modules/financial-accounts/resolvers/financial-accounts.resolver.ts
+++ packages/server/src/modules/financial-accounts/resolvers/financial-accounts.resolver.ts
@@ -1,5 +1,10 @@
+import { GraphQLError } from 'graphql';
 import { FinancialAccountsProvider } from '../providers/financial-accounts.provider.js';
-import type { FinancialAccountsModule } from '../types.js';
+import type {
+  FinancialAccountsModule,
+  IInsertFinancialAccountsParams,
+  IUpdateFinancialAccountParams,
+} from '../types.js';
 import {
   commonFinancialAccountFields,
   commonFinancialEntityFields,
@@ -11,28 +16,96 @@ export const financialAccountsResolvers: FinancialAccountsModule.Resolvers = {
     allFinancialAccounts: async (_, __, { injector }) => {
       return injector.get(FinancialAccountsProvider).getAllFinancialAccounts();
     },
+    financialAccount: async (_, { id }, { injector }) => {
+      return injector
+        .get(FinancialAccountsProvider)
+        .getFinancialAccountByAccountIDLoader.load(id)
+        .then(account => {
+          if (!account) {
+            throw new GraphQLError('Financial account not found');
+          }
+          return account;
+        });
+    },
+  },
+  Mutation: {
+    deleteFinancialAccount: async (_, { id }, { injector }) => {
+      return injector
+        .get(FinancialAccountsProvider)
+        .deleteFinancialAccount({ financialAccountId: id })
+        .then(() => true);
+    },
+    createFinancialAccount: async (_, { input }, { injector }) => {
+      try {
+        const bankAccount: IInsertFinancialAccountsParams['bankAccounts'][number] = {
+          accountNumber: input.number,
+          name: input.name,
+          privateBusiness: input.privateOrBusiness,
+          ownerId: input.ownerId,
+          type: input.type,
+        };
+        const [account] = await injector
+          .get(FinancialAccountsProvider)
+          .insertFinancialAccounts({ bankAccounts: [bankAccount] });
+
+        if (input.bankAccountDetails) {
+          // TODO: Update bank account details
+        }
+
+        return account;
+      } catch (error) {
+        const message = 'Failed to create financial account';
+        console.error(message, error);
+        throw new GraphQLError(message);
+      }
+    },
+    updateFinancialAccount: async (_, { id, fields }, { injector }) => {
+      try {
+        const updatedAccount: IUpdateFinancialAccountParams = {
+          financialAccountId: id,
+          accountNumber: fields.number,
+          ownerId: fields.ownerId,
+          privateBusiness: fields.privateOrBusiness,
+          type: fields.type,
+        };
+        const account = await injector
+          .get(FinancialAccountsProvider)
+          .updateFinancialAccount(updatedAccount);
+
+        if (fields.bankAccountDetails) {
+          // TODO: Update bank account details
+        }
+
+        return account;
+      } catch (error) {
+        const message = 'Failed to update financial account';
+        console.error(message, error);
+        throw new GraphQLError(message);
+      }
+    },
   },
   CardFinancialAccount: {
     __isTypeOf: DbAccount => DbAccount.type === 'CREDIT_CARD',
     ...commonFinancialAccountFields,
     number: DbAccount => DbAccount.account_number,
     fourDigits: DbAccount => DbAccount.account_number,
-    name: DbAccount => DbAccount.account_number,
+    name: DbAccount => DbAccount.account_name ?? DbAccount.account_number,
   },
   CryptoWalletFinancialAccount: {
     __isTypeOf: DbAccount => DbAccount.type === 'CRYPTO_WALLET',
     ...commonFinancialAccountFields,
     number: DbAccount => DbAccount.account_number,
     name: DbAccount =>
-      DbAccount.account_number.length >= 20
+      DbAccount.account_name ??
+      (DbAccount.account_number.length >= 20
         ? DbAccount.account_number.slice(-8)
-        : DbAccount.account_number,
+        : DbAccount.account_number),
   },
   ForeignSecuritiesFinancialAccount: {
     __isTypeOf: DbAccount => DbAccount.type === 'FOREIGN_SECURITIES',
     ...commonFinancialAccountFields,
     number: DbAccount => DbAccount.account_number,
-    name: DbAccount => DbAccount.account_number,
+    name: DbAccount => DbAccount.account_name ?? DbAccount.account_number,
   },
   ConversionTransaction: {
     ...commonTransactionFields,
