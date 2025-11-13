6c27b05d9c308f60ac669a8b1776ce0145d96bd9
Gil Gardosh <gilgardosh@gmail.com>
2025-11-14 01:46:33 +0200

    refactor annual revenue DB query

--- packages/server/src/modules/reports/providers/annual-revenue-report.provider.ts
+++ packages/server/src/modules/reports/providers/annual-revenue-report.provider.ts
@@ -2,62 +2,198 @@ import { Injectable, Scope } from 'graphql-modules';
 import { DBProvider } from '@modules/app-providers/db.provider.js';
 import { sql } from '@pgtyped/runtime';
 // import { getCacheInstance } from '@shared/helpers';
-import {
-  IGetNormalizedRevenueTransactionsParams,
-  IGetNormalizedRevenueTransactionsQuery,
-} from '../types.js';
+import { IGetNormalizedRevenueRecordsParams, IGetNormalizedRevenueRecordsQuery } from '../types.js';
 
-const getNormalizedRevenueTransactions = sql<IGetNormalizedRevenueTransactionsQuery>`
-with transactions AS (SELECT
-    t.*,
-    c.owner_id,
-    COALESCE(t.debit_date_override, t.debit_timestamp, t.debit_date) as normalized_debit_date,
-    CASE
-        WHEN t.currency = 'USD' THEN t.amount
-        WHEN t.currency = 'ILS' THEN t.amount / lr.usd -- Convert ILS => USD
-        WHEN t.currency = 'AUD' THEN t.amount * (lr.aud / lr.usd) -- Convert AUD => ILS => USD
-        WHEN t.currency = 'CAD' THEN t.amount * (lr.cad / lr.usd) -- Convert CAD => ILS => USD
-        WHEN t.currency = 'EUR' THEN t.amount * (lr.eur / lr.usd) -- Convert EUR => ILS => USD
-        WHEN t.currency = 'GBP' THEN t.amount * (lr.gbp / lr.usd) -- Convert GBP => ILS => USD
-        WHEN t.currency = 'JPY' THEN t.amount * (lr.jpy / lr.usd) -- Convert JPY => ILS => USD
-        WHEN t.currency = 'SEK' THEN t.amount * (lr.sek / lr.usd) -- Convert SEK => ILS => USD
-        WHEN t.currency = 'USDC' OR t.currency = 'GRT' OR t.currency = 'ETH' THEN t.amount * lr2.value -- Convert Crypto => USD
-        ELSE NULL
-    END AS amount_usd,
-        CASE
-        WHEN t.currency = 'ILS' THEN t.amount
-        WHEN t.currency = 'USD' THEN t.amount * lr.usd -- Convert USD => ILS
-        WHEN t.currency = 'AUD' THEN t.amount * lr.aud -- Convert AUD => ILS
-        WHEN t.currency = 'CAD' THEN t.amount * lr.cad -- Convert CAD => ILS
-        WHEN t.currency = 'EUR' THEN t.amount * lr.eur -- Convert EUR => ILS
-        WHEN t.currency = 'GBP' THEN t.amount * lr.gbp -- Convert GBP => ILS
-        WHEN t.currency = 'JPY' THEN t.amount * lr.jpy -- Convert JPY => ILS
-        WHEN t.currency = 'SEK' THEN t.amount * lr.sek -- Convert SEK => ILS
-        WHEN t.currency = 'USDC' OR t.currency = 'GRT' OR t.currency = 'ETH' THEN t.amount * lr2.value * lr.usd -- Convert Crypto => USD => ILS
-        ELSE NULL
-    END AS amount_ils
-FROM accounter_schema.transactions t
-LEFT JOIN accounter_schema.charges c ON t.charge_id = c.id
-LEFT JOIN LATERAL (
-    SELECT er.usd, er.eur, er.gbp, er.cad, er.jpy, er.aud, er.sek
-    FROM accounter_schema.exchange_rates er
-    WHERE er.exchange_date <= COALESCE(t.debit_date_override, t.debit_timestamp, t.debit_date)
-    ORDER BY er.exchange_date DESC
-    LIMIT 1
-) lr ON TRUE
-LEFT JOIN LATERAL (
-    SELECT cer.value
-    FROM accounter_schema.crypto_exchange_rates cer
-    WHERE cer.date <= t.debit_timestamp AND cer.coin_symbol::accounter_schema.currency = t.currency AND cer.against = 'USD'
-    ORDER BY cer.date DESC
-    LIMIT 1
-) lr2 ON t.debit_timestamp IS NOT NULL)
-SELECT t.*
-FROM transactions t
-WHERE t.owner_id = $ownerId
-AND t.normalized_debit_date BETWEEN $fromDate AND $toDate
-AND ($isBusinessIDs = 0 OR t.business_id IN $$businessIDs)
-ORDER BY t.normalized_debit_date;`;
+const getNormalizedRevenueRecords = sql<IGetNormalizedRevenueRecordsQuery>`
+WITH
+  normalizedLedgerRevenue AS (
+    WITH
+      ChargeBusinessID AS (
+        -- 1. Gather potential Business IDs from transactions and documents
+        SELECT
+          t.charge_id,
+          CASE
+            WHEN array_length(ARRAY_AGG(DISTINCT potential_id), 1) = 1 THEN ARRAY_AGG(DISTINCT potential_id)
+            ELSE NULL
+          END AS entity_id
+        FROM
+          (
+            -- IDs from 'transactions' table (if not a fee and not null)
+            SELECT
+              charge_id,
+              business_id AS potential_id
+            FROM
+              accounter_schema.transactions
+            WHERE
+              NOT is_fee
+              AND business_id IS NOT NULL
+            UNION ALL
+            -- IDs from 'documents' table (creditor_id if not OWNER_ID and not null)
+            SELECT
+              charge_id,
+              creditor_id AS potential_id
+            FROM
+              accounter_schema.documents
+            WHERE
+              creditor_id IS NOT NULL
+              AND creditor_id <> $ownerId -- OWNER_ID constant
+            UNION ALL
+            -- IDs from 'documents' table (debtor_id if not OWNER_ID and not null)
+            SELECT
+              charge_id,
+              debtor_id AS potential_id
+            FROM
+              accounter_schema.documents
+            WHERE
+              debtor_id IS NOT NULL
+              AND debtor_id <> $ownerId -- OWNER_ID constant
+          ) t
+        GROUP BY
+          t.charge_id
+      )
+    SELECT
+      ledger_records.id,
+      ledger_records.charge_id,
+      ledger_records.owner_id,
+      ledger_records.debit_local_amount1 as amount_local,
+      ledger_records.debit_foreign_amount1 as amount_foreign,
+      ledger_records.currency as currency,
+      ledger_records.value_date as date,
+      ledger_records.reference1 as reference,
+      ledger_records.description as description,
+      ledger_records.debit_entity1 as business_id,
+      b.country as country
+    FROM
+      accounter_schema.ledger_records
+      LEFT JOIN accounter_schema.businesses b ON b.id = ledger_records.debit_entity1
+    WHERE
+      (
+        ledger_records.credit_entity1 IN $$incomeTaxCategoriesIDs
+        OR ledger_records.credit_entity2 IN $$incomeTaxCategoriesIDs
+      )
+      AND ledger_records.owner_id = $ownerId
+      AND ledger_records.value_date BETWEEN $fromDate AND $toDate
+    UNION
+    SELECT
+      ledger_records.id,
+      ledger_records.charge_id,
+      ledger_records.owner_id,
+      ledger_records.credit_local_amount1 as amount_local,
+      ledger_records.credit_foreign_amount1 as amount_foreign,
+      ledger_records.currency as currency,
+      ledger_records.value_date as date,
+      ledger_records.reference1 as reference,
+      ledger_records.description as description,
+      ledger_records.credit_entity1 as business_id,
+      b.country as country
+    FROM
+      accounter_schema.ledger_records
+      LEFT JOIN accounter_schema.businesses b ON b.id = ledger_records.credit_entity1
+    WHERE
+      (
+        ledger_records.debit_entity1 IN $$incomeTaxCategoriesIDs
+        OR ledger_records.debit_entity2 IN $$incomeTaxCategoriesIDs
+      )
+      AND ledger_records.owner_id = $ownerId
+      AND ledger_records.value_date BETWEEN $fromDate AND $toDate
+    UNION
+    SELECT
+      ledger_records.id,
+      ledger_records.charge_id,
+      ledger_records.owner_id,
+      ledger_records.debit_local_amount1 * -1 as amount_local,
+      ledger_records.debit_foreign_amount1 * -1 as amount_foreign,
+      ledger_records.currency as currency,
+      ledger_records.invoice_date as date,
+      ledger_records.reference1 as reference,
+      ledger_records.description as description,
+      cbi.entity_id[1] as business_id,
+      b.country as country
+    FROM
+      accounter_schema.ledger_records
+      LEFT JOIN ChargeBusinessID cbi ON ledger_records.charge_id = cbi.charge_id
+      LEFT JOIN accounter_schema.businesses b ON b.id = cbi.entity_id[1]
+    WHERE
+      (
+        ledger_records.credit_entity1 = $incomeToCollectId
+        OR ledger_records.credit_entity2 = $incomeToCollectId
+      )
+      AND ledger_records.owner_id = $ownerId
+      AND ledger_records.invoice_date = $fromDate
+    UNION
+    SELECT
+      ledger_records.id,
+      ledger_records.charge_id,
+      ledger_records.owner_id,
+      ledger_records.credit_local_amount1 as amount_local,
+      ledger_records.credit_foreign_amount1 as amount_foreign,
+      ledger_records.currency as currency,
+      ledger_records.invoice_date as date,
+      ledger_records.reference1 as reference,
+      ledger_records.description as description,
+      cbi.entity_id[1] as business_id,
+      b.country as country
+    FROM
+      accounter_schema.ledger_records
+      LEFT JOIN ChargeBusinessID cbi ON ledger_records.charge_id = cbi.charge_id
+      LEFT JOIN accounter_schema.businesses b ON b.id = cbi.entity_id[1]
+    WHERE
+      (
+        ledger_records.debit_entity1 = $incomeToCollectId
+        OR ledger_records.debit_entity2 = $incomeToCollectId
+      )
+      AND ledger_records.owner_id = $ownerId
+      AND ledger_records.invoice_date = $toDate
+  )
+SELECT
+  nlr.*,
+  CASE
+    WHEN nlr.currency = 'USD' THEN nlr.amount_foreign
+    WHEN nlr.currency = 'ILS' THEN nlr.amount_local / lr.usd -- Convert ILS => USD    
+    WHEN nlr.currency = 'AUD' THEN nlr.amount_foreign * (lr.aud / lr.usd) -- Convert AUD => ILS => USD    
+    WHEN nlr.currency = 'CAD' THEN nlr.amount_foreign * (lr.cad / lr.usd) -- Convert CAD => ILS => USD     
+    WHEN nlr.currency = 'EUR' THEN nlr.amount_foreign * (lr.eur / lr.usd) -- Convert EUR => ILS => USD      
+    WHEN nlr.currency = 'GBP' THEN nlr.amount_foreign * (lr.gbp / lr.usd) -- Convert GBP => ILS => USD      
+    WHEN nlr.currency = 'JPY' THEN nlr.amount_foreign * (lr.jpy / lr.usd) -- Convert JPY => ILS => USD      
+    WHEN nlr.currency = 'SEK' THEN nlr.amount_foreign * (lr.sek / lr.usd) -- Convert SEK => ILS => USD      
+    WHEN nlr.currency = 'USDC'
+    OR nlr.currency = 'GRT'
+    OR nlr.currency = 'ETH' THEN nlr.amount_foreign * lr2.value -- Convert Crypto => USD  
+  END AS amount_usd
+FROM
+  normalizedLedgerRevenue nlr
+  LEFT JOIN LATERAL (
+    SELECT
+      er.usd,
+      er.eur,
+      er.gbp,
+      er.cad,
+      er.jpy,
+      er.aud,
+      er.sek
+    FROM
+      accounter_schema.exchange_rates er
+    WHERE
+      er.exchange_date <= nlr.date
+    ORDER BY
+      er.exchange_date DESC
+    LIMIT
+      1
+  ) lr ON TRUE
+  LEFT JOIN LATERAL (
+    SELECT
+      cer.value
+    FROM
+      accounter_schema.crypto_exchange_rates cer
+    WHERE
+      cer.date <= nlr.date
+      AND cer.coin_symbol::accounter_schema.currency = nlr.currency
+      AND cer.against = 'USD'
+    ORDER BY
+      cer.date DESC
+    LIMIT
+      1
+  ) lr2 ON nlr.date IS NOT NULL;`;
 
 @Injectable({
   scope: Scope.Singleton,
@@ -70,9 +206,9 @@ export class AnnualRevenueReportProvider {
 
   constructor(private dbProvider: DBProvider) {}
 
-  public async getNormalizedRevenueTransactions(params: IGetNormalizedRevenueTransactionsParams) {
+  public async getNormalizedRevenueRecords(params: IGetNormalizedRevenueRecordsParams) {
     try {
-      return getNormalizedRevenueTransactions.run(params, this.dbProvider);
+      return getNormalizedRevenueRecords.run(params, this.dbProvider);
     } catch (error) {
       const message = 'Failed to get balance transactions';
       console.error(`${message}: ${error}`);
