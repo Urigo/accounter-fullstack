3060c32c91553669461f4c695b2b951bbf10c703
Gil Gardosh <gilgardosh@gmail.com>
2025-11-16 18:08:14 +0200

    essential helpers

---
+++ packages/server/src/modules/documents/helpers/common.helper.ts
@@ -0,0 +1,14 @@
+import { DocumentType } from '@shared/enums';
+import { document_type } from '../types.js';
+
+export function isInvoice(type: document_type): boolean {
+  return (
+    type === DocumentType.Invoice ||
+    type === DocumentType.CreditInvoice ||
+    type === DocumentType.InvoiceReceipt
+  );
+}
+
+export function isReceipt(type: document_type): boolean {
+  return type === DocumentType.Receipt || type === DocumentType.InvoiceReceipt;
+}
