fa0581150dc4a4910609debb477772e473b9170b
Gil Gardosh <gilgardosh@gmail.com>
2025-11-30 13:16:20 +0200

    prettier

--- packages/client/src/components/common/inputs/multi-select.tsx
+++ packages/client/src/components/common/inputs/multi-select.tsx
@@ -48,8 +48,7 @@ const multiSelectVariants = cva(
  * Props for MultiSelect component
  */
 interface MultiSelectProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof multiSelectVariants> {
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof multiSelectVariants> {
   /**
    * An array of option objects to be displayed in the multi-select component.
    * Each option object has a label, value, and an optional icon.
