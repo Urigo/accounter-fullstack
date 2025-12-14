b7ad98aabf547c6047ddeb95bb94bd6b34ad3322
Gil Gardosh <gilgardosh@gmail.com>
2025-12-14 12:01:20 +0200

    export types

---
+++ packages/modern-poalim-scraper/src/zod-schemas/types.ts
@@ -0,0 +1,4 @@
+export type { IsracardCardsTransactionsList } from './isracard-cards-transactions-list-schema.js';
+export type { IsracardDashboardMonth } from './isracard-dashboard-month-schema.js';
+export type { SwiftTransaction } from './swift-transaction-schema.js';
+export type { SwiftTransactions } from './swift-transactions-schema.js';
