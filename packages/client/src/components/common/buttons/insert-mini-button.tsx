155f2521a998a6214f3489ba49801dd5d377dbb2
Gil Gardosh <gilgardosh@gmail.com>
2025-12-21 19:00:23 +0200

    .

--- packages/client/src/components/common/buttons/insert-mini-button.tsx
+++ packages/client/src/components/common/buttons/insert-mini-button.tsx
@@ -2,9 +2,7 @@ import type { ComponentProps, ReactElement } from 'react';
 import { Plus } from 'lucide-react';
 import { Button } from '../../ui/button.js';
 
-export function InsertMiniButton(
-  props: ComponentProps<typeof Button> & { tooltip?: string },
-): ReactElement {
+export function InsertMiniButton(props: ComponentProps<typeof Button>): ReactElement {
   return (
     <Button variant="ghost" size="icon" className="size-7.5" {...props}>
       <Plus className="size-5" />
