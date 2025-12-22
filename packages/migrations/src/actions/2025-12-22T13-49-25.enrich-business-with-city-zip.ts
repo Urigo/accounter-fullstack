7d20e9f0bad1e5082a2e0f8bb8fcc558ad503fc6
Gil Gardosh <gilgardosh@gmail.com>
2025-12-22 15:39:10 +0200

    .

--- packages/migrations/src/actions/2025-12-22T13-49-25.enrich-business-with-city-zip.ts
+++ packages/migrations/src/actions/2025-12-22T13-49-25.enrich-business-with-city-zip.ts
@@ -9,5 +9,49 @@ export default {
       "city" VARCHAR(50) NULL,
     ADD COLUMN
       "zip_code" VARCHAR(15) NULL;
+    DROP COLUMN
+      "password",
+    DROP COLUMN
+      "tax_siduri_number_2021",
+    DROP COLUMN
+      "username_vat_website",
+    DROP COLUMN
+      "website_login_screenshot",
+    DROP COLUMN
+      "pinkas_social_security_2021",
+    DROP COLUMN
+      "tax_pinkas_number_2020",
+    DROP COLUMN
+      "wizcloud_token",
+    DROP COLUMN
+      "wizcloud_company_id",
+    DROP COLUMN
+      "advance_tax_rate",
+    DROP COLUMN
+      "bank_account_bank_number",
+    DROP COLUMN
+      "bank_account_branch_number",
+    DROP COLUMN
+      "bank_account_account_number",
+    DROP COLUMN
+      "bank_account_IBAN",
+    DROP COLUMN
+      "tax_nikuim_pinkas_number",
+    DROP COLUMN
+      "bank_account_swift",
+    DROP COLUMN
+      "vat_report_cadence",
+    DROP COLUMN
+      "contract",
+    DROP COLUMN
+      "tax_siduri_number_2022",
+    DROP COLUMN
+      "pinkas_social_security_2022",
+    DROP COLUMN
+      "registration_date",
+    DROP COLUMN
+      "is_authority",
+    DROP COLUMN
+      "nikuim";
   `,
 } satisfies MigrationExecutor;
