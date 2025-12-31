f3e1bca87442059ed2fe510bdd9b4a6b017e24a4
Gil Gardosh <gilgardosh@gmail.com>
2025-12-31 18:13:26 +0200

    cleanup

--- packages/scraper-local-app/scripts/set-db-url.ts
+++ packages/scraper-local-app/scripts/set-db-url.ts
@@ -1,6 +1,6 @@
 // returns the database URL
 // This is a script used for later setting DATABASE_URL environment variable for the PgTyped dependency.
-import { config as appConfig } from '../src/env.js';
+import { config as appConfig } from '../src/env.ts';
 
 const { user, password, host, port, database, ssl } = appConfig?.database ?? {};
 
