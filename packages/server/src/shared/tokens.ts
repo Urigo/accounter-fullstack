0d0f062723c99bffe6df3996b1f805ba91584d2c
Gil Gardosh <gilgardosh@gmail.com>
2026-01-28 12:55:10 +0200

    User accounts migration - phase2 - Auth Context Provider

--- packages/server/src/shared/tokens.ts
+++ packages/server/src/shared/tokens.ts
@@ -1,6 +1,9 @@
 import { InjectionToken } from 'graphql-modules';
+import type { RawAuth } from '../plugins/auth-plugin-v2.js';
 import type { AuthContext } from './types/auth.js';
 import type { Environment } from './types/index.js';
 
 export const ENVIRONMENT = new InjectionToken<Environment>('ENVIRONMENT');
 export const AUTH_CONTEXT = new InjectionToken<AuthContext>('AUTH_CONTEXT');
+export const AUTH_CONTEXT_V2 = new InjectionToken<AuthContext>('AUTH_CONTEXT_V2');
+export const RAW_AUTH = new InjectionToken<RawAuth>('RAW_AUTH');
