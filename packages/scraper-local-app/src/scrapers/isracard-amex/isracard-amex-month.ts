c74fd898e9caa3bf9461845b589a1c07887eb63c
Gil Gardosh <gilgardosh@gmail.com>
2026-02-15 18:49:23 +0200

    Handle `null` months data on Isracard scraper (#3055)
    * Handle `null` months data on Isracard scraper
    
    * .

--- packages/scraper-local-app/src/scrapers/isracard-amex/isracard-amex-month.ts
+++ packages/scraper-local-app/src/scrapers/isracard-amex/isracard-amex-month.ts
@@ -478,6 +478,18 @@ export async function getMonthTransactions(
           try {
             const monthTransactions = await scraper!.getMonthTransactions(month);
 
+            if (!monthTransactions.data) {
+              if ('errors' in monthTransactions && monthTransactions.errors) {
+                logger.error('Validation errors on month transactions', monthTransactions.errors);
+              }
+              task.skip('No data');
+              ctx[accountKey].processedData ??= {};
+              ctx[accountKey].processedData.transactions ??= 0;
+              parentTask.title = originalTitle + ' (No data)';
+              logger.error('No data received from scraper', { month: format(month, 'MM-yyyy') });
+              return;
+            }
+
             if (!monthTransactions.isValid) {
               if ('errors' in monthTransactions) {
                 logger.error(monthTransactions.errors);
@@ -485,14 +497,6 @@ export async function getMonthTransactions(
               throw new Error(
                 `Invalid transactions data for ${type} ${nickname} ${format(month, 'MM-yyyy')}`,
               );
-            }
-
-            if (!monthTransactions.data) {
-              task.skip('No data');
-              ctx[accountKey].processedData ??= {};
-              ctx[accountKey].processedData.transactions ??= 0;
-              parentTask.title = originalTitle + ' (No data)';
-              return;
             }
 
             if (monthTransactions?.data?.Header?.Status !== '1') {
