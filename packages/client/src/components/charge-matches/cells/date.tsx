29c0d00741f520df9de3b6ba1deec0594fd621f9
Gil Gardosh <gilgardosh@gmail.com>
2026-02-15 10:04:28 +0200

    Align charge matches table date format

--- packages/client/src/components/charge-matches/cells/date.tsx
+++ packages/client/src/components/charge-matches/cells/date.tsx
@@ -1,13 +1,10 @@
 import type { ReactElement } from 'react';
+import { formatTimelessDateString } from '@/helpers/index.js';
 
 type Props = {
   date?: Date;
 };
 
 export const DateCell = ({ date }: Props): ReactElement => {
-  return (
-    <p className="text-sm font-medium">
-      {date?.toLocaleDateString(undefined, { timeZone: 'UTC' })}
-    </p>
-  );
+  return <p className="text-sm font-medium">{date ? formatTimelessDateString(date) : ''}</p>;
 };
