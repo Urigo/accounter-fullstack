161c8f9a5bc755a488b1a14125c2c14425765e59
Gil Gardosh <gilgardosh@gmail.com>
2026-04-06 18:58:37 +0300

    Adjust green invoice to API recent updates (#3286)

--- packages/green-invoice-graphql/src/index.ts
+++ packages/green-invoice-graphql/src/index.ts
@@ -1,20 +1,37 @@
 import { getMeshSDK, Sdk } from './mesh-artifacts/index.js';
 
+async function obtainAccessToken(id: string, secret: string) {
+  try {
+    const authParams = {
+      grant_type: 'client_credentials',
+      client_id: id,
+      client_secret: secret,
+    };
+    const res = await fetch('https://api.morning.co/idp/v1/oauth/token', {
+      method: 'POST',
+      body: JSON.stringify(authParams),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (res.status !== 200) {
+      throw Error(
+        `Failed to obtain access token, status code: ${res.status}, response: ${await res.text()}`,
+      );
+    }
+
+    const data = await res.json();
+    return data.accessToken;
+  } catch (e) {
+    console.error('Failed to obtain access token', e);
+    throw Error('Failed to obtain access token', { cause: e });
+  }
+}
+
 export const init = async (
   id: string,
   secret: string,
 ): Promise<{ sdk: Sdk; authToken: string }> => {
-  const authParams = {
-    id,
-    secret,
-  };
-  const greenInvoiceToken = await fetch('https://api.greeninvoice.co.il/api/v1/account/token', {
-    method: 'POST',
-    body: JSON.stringify(authParams),
-    headers: { 'Content-Type': 'application/json' },
-  })
-    .then(res => res.json())
-    .then((res: { token: string }) => res.token);
+  const greenInvoiceToken = await obtainAccessToken(id, secret);
 
   const sdk = await getMeshSDK({ authToken: greenInvoiceToken });
 
