fb0833c64cd53a0ca3c3678e7df9b60455b5feaf
Gil Gardosh <gilgardosh@gmail.com>
2026-06-24 16:04:33 +0300

    .

--- packages/server/src/modules/email-ingestion/types.ts
+++ packages/server/src/modules/email-ingestion/types.ts
@@ -1,4 +1,5 @@
 export type * from './__generated__/types.js';
+export type { DateOrString } from './__generated__/email-ingestion-control.types.js';
 export type * from './__generated__/email-ingestion-control.types.js';
 export type * from './__generated__/email-ingestion-alias.types.js';
 export type * from './__generated__/email-ingestion-ingest.types.js';
